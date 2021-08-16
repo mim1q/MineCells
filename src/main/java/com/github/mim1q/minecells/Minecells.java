fcea8cb24ed74755d3d2ef04c4fe968c44d8717d
Mim1q <mim1q.dev@gmail.com>
2021-08-16 17:27:05 +0200

    Setup project

---
+++ src/main/java/com/github/mim1q/minecells/Minecells.java
@@ -0,0 +1,10 @@
+package com.github.mim1q.minecells;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Minecells implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
