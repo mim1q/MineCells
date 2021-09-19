6e261e7a691a9751897178545fd38a9751d458e2
Mim1q <mim1q.dev@gmail.com>
2021-09-19 18:28:53 +0200

    Added Sounds

---
+++ src/main/java/com/github/mim1q/minecells/registry/SoundRegistry.java
@@ -0,0 +1,20 @@
+package com.github.mim1q.minecells.registry;
+
+import com.github.mim1q.minecells.MineCells;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+
+public final class SoundRegistry {
+    public static final Identifier JUMPING_ZOMBIE_MELEE_ID = new Identifier("minecells:jumping_zombie.melee");
+    public static final SoundEvent JUMPING_ZOMBIE_MELEE_SOUND_EVENT = new SoundEvent(JUMPING_ZOMBIE_MELEE_ID);
+
+    public static final Identifier JUMPING_ZOMBIE_JUMP_ID = new Identifier("minecells:jumping_zombie.jump");
+    public static final SoundEvent JUMPING_ZOMBIE_JUMP_SOUND_EVENT = new SoundEvent(JUMPING_ZOMBIE_JUMP_ID);
+
+    public static void register() {
+        Registry.register(Registry.SOUND_EVENT, JUMPING_ZOMBIE_MELEE_ID, JUMPING_ZOMBIE_MELEE_SOUND_EVENT);
+        Registry.register(Registry.SOUND_EVENT, JUMPING_ZOMBIE_JUMP_ID, JUMPING_ZOMBIE_JUMP_SOUND_EVENT);
+    }
+}
