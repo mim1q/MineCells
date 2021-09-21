7b415acf5dee37057f7a4985a60bc6605e73661a
Mim1q <mim1q.dev@gmail.com>
2021-09-21 17:52:52 +0200

    Update WalkTowardsTargetGoal.java
    made this simpler and better

--- src/main/java/com/github/mim1q/minecells/entity/ai/goal/WalkTowardsTargetGoal.java
+++ src/main/java/com/github/mim1q/minecells/entity/ai/goal/WalkTowardsTargetGoal.java
@@ -1,43 +1,15 @@
 package com.github.mim1q.minecells.entity.ai.goal;
 
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.ai.goal.Goal;
-import net.minecraft.entity.mob.HostileEntity;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.entity.mob.PathAwareEntity;
 
-import java.util.EnumSet;
+public class WalkTowardsTargetGoal extends MeleeAttackGoal {
 
-public class WalkTowardsTargetGoal extends Goal {
-
-    protected HostileEntity entity;
-
-    public WalkTowardsTargetGoal(HostileEntity entity) {
-        this.setControls(EnumSet.of(Control.MOVE, Control.LOOK));
-        this.entity = entity;
+    public WalkTowardsTargetGoal(PathAwareEntity mob, double speed, boolean pauseWhenMobIdle) {
+        super(mob, speed, pauseWhenMobIdle);
     }
 
     @Override
-    public boolean canStart() {
-        return this.entity.getTarget() != null;
-    }
-
-    @Override
-    public boolean shouldContinue() {
-        LivingEntity target = this.entity.getTarget();
-        return target != null && !target.isSpectator() && !((PlayerEntity)target).isCreative();
-    }
-
-    @Override
-    public void stop() {
-        this.entity.getNavigation().stop();
-    }
-
-    @Override
-    public void tick() {
-        LivingEntity target = this.entity.getTarget();
-        if(target != null) {
-            this.entity.getLookControl().lookAt(target);
-            this.entity.getNavigation().startMovingTo(target, 0.6d);
-        }
-    }
+    public void attack(LivingEntity target, double squaredDistance) { }
 }
