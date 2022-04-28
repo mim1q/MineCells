6e0b44a67b5a1ddfed8468d8e1c074609612a2b0
Mim1q <mim1q.dev@gmail.com>
2022-04-28 08:27:21 +0200

    Changed nbt naming to match base game's

--- src/main/java/com/github/mim1q/minecells/entity/projectile/GrenadeEntity.java
+++ src/main/java/com/github/mim1q/minecells/entity/projectile/GrenadeEntity.java
@@ -1,5 +1,14 @@
 package com.github.mim1q.minecells.entity.projectile;
 
+import com.mojang.datafixers.util.Pair;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import it.unimi.dsi.fastutil.objects.ObjectListIterator;
+import net.minecraft.block.AbstractFireBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.MovementType;
 import net.minecraft.entity.damage.DamageSource;
@@ -7,10 +16,23 @@ import net.minecraft.entity.data.DataTracker;
 import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.loot.context.LootContext;
+import net.minecraft.loot.context.LootContextParameters;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraft.world.explosion.Explosion;
+import net.minecraft.world.explosion.ExplosionBehavior;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collections;
+import java.util.Iterator;
 
 public class GrenadeEntity extends ProjectileEntity {
     private Vec3d shootVector;
@@ -77,13 +99,13 @@ public class GrenadeEntity extends ProjectileEntity {
     @Override
     public void readCustomDataFromNbt(NbtCompound nbt) {
         super.readCustomDataFromNbt(nbt);
-        this.setFuse(nbt.getInt("fuse"));
+        this.setFuse(nbt.getInt("Fuse"));
     }
 
     @Override
     public void writeCustomDataToNbt(NbtCompound nbt) {
         super.writeCustomDataToNbt(nbt);
-        nbt.putInt("fuse", this.getFuse());
+        nbt.putInt("Fuse", this.getFuse());
     }
 
     public static class GrenadeDamageSource extends DamageSource {
