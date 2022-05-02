4c0674c04c7311445d003270b4b9df85248c3438
Mim1q <mim1q.dev@gmail.com>
2022-05-02 19:33:29 +0200

    Assassin's dagger spawns particles when it crits

--- src/main/java/com/github/mim1q/minecells/item/AssassinsDaggerItem.java
+++ src/main/java/com/github/mim1q/minecells/item/AssassinsDaggerItem.java
@@ -1,6 +1,10 @@
 package com.github.mim1q.minecells.item;
 
+import com.github.mim1q.minecells.network.PacketHandler;
 import com.github.mim1q.minecells.registry.SoundRegistry;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
+import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
@@ -8,6 +12,9 @@ import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ToolItem;
 import net.minecraft.item.ToolMaterials;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
@@ -24,19 +31,22 @@ public class AssassinsDaggerItem extends ToolItem {
         float difference = MathHelper.angleBetween(target.bodyYaw, attacker.getHeadYaw());
         if (difference < 60.0F) {
             attacker.world.playSound(null, attacker.getX(), attacker.getY(), attacker.getZ(), SoundRegistry.CRIT, SoundCategory.PLAYERS, 0.5F, 1.0F);
+            for(ServerPlayerEntity player : PlayerLookup.around((ServerWorld)target.world, target.getPos(), 30.0D)) {
+                PacketByteBuf buf = PacketByteBufs.create();
+                buf.writeDouble(target.getX());
+                buf.writeDouble(target.getY() + 1.5D);
+                buf.writeDouble(target.getZ());
+                ServerPlayNetworking.send(player, PacketHandler.CRIT, buf);
+            }
             target.damage(DamageSource.mob(attacker), this.getDamage());
         }
-        stack.damage(1, attacker, (e) -> {
-            e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND);
-        });
+        stack.damage(1, attacker, (e) -> e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
         return true;
     }
 
     @Override
     public boolean postMine(ItemStack stack, World world, BlockState state, BlockPos pos, LivingEntity miner) {
-        stack.damage(2, miner, (e) -> {
-            e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND);
-        });
+        stack.damage(2, miner, (e) -> e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
         return true;
     }
 
