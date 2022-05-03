6c2d725d3e824d3fd5a93c55382acebd85b9797a
Mim1q <mim1q.dev@gmail.com>
2022-05-03 13:49:34 +0200

    Added inquisitor spawn egg to item tab

--- src/main/java/com/github/mim1q/minecells/client/renderer/JumpingZombieEntityRenderer.java
+++ src/main/java/com/github/mim1q/minecells/client/renderer/JumpingZombieEntityRenderer.java
@@ -3,13 +3,19 @@ package com.github.mim1q.minecells.client.renderer;
 import com.github.mim1q.minecells.client.model.JumpingZombieEntityModel;
 import com.github.mim1q.minecells.client.renderer.layer.JumpingZombieGlowLayer;
 import com.github.mim1q.minecells.entity.JumpingZombieEntity;
+import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.util.Identifier;
 import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 
-public class JumpingZombieEntityRenderer extends GeoEntityRenderer<JumpingZombieEntity> {
-    public JumpingZombieEntityRenderer(EntityRendererFactory.Context context) {
-        super(context, new JumpingZombieEntityModel());
-        this.addLayer(new JumpingZombieGlowLayer(this));
-        this.shadowRadius = 0.35f;
+public class JumpingZombieEntityRenderer extends EntityRenderer<JumpingZombieEntity> {
+
+    protected JumpingZombieEntityRenderer(EntityRendererFactory.Context ctx) {
+        super(ctx);
+    }
+
+    @Override
+    public Identifier getTexture(JumpingZombieEntity entity) {
+        return null;
     }
 }
