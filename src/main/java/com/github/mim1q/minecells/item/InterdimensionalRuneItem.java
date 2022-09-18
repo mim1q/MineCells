3c0c899c4932d4a584caa65769c582d9b85e0a6a
mim1q <mim1q.dev@gmail.com>
2022-09-18 14:28:01 +0200

    Added tooltips and names to rune item

--- src/main/java/com/github/mim1q/minecells/item/InterdimensionalRuneItem.java
+++ src/main/java/com/github/mim1q/minecells/item/InterdimensionalRuneItem.java
@@ -2,13 +2,18 @@ package com.github.mim1q.minecells.item;
 
 import com.github.mim1q.minecells.accessor.PlayerEntityAccessor;
 import com.github.mim1q.minecells.registry.MineCellsItems;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsage;
+import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class InterdimensionalRuneItem extends Item {
   public InterdimensionalRuneItem(Settings settings) {
@@ -36,5 +41,14 @@ public class InterdimensionalRuneItem extends Item {
       return TypedActionResult.success(stack, true);
     }
     return TypedActionResult.pass(stack);
+  }
+
+  @Override
+  public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+    super.appendTooltip(stack, world, tooltip, context);
+    final int maxCells = stack.getMaxDamage();
+    final int cells = maxCells - stack.getDamage();
+    tooltip.add(Text.translatable("item.minecells.interdimensional_rune.tooltip"));
+    tooltip.add(Text.translatable("item.minecells.interdimensional_rune.tooltip2", cells, maxCells));
   }
 }
