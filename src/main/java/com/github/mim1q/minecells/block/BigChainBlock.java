0aca0c6edbef11e2d7271f1b60c871429c7626e0
mim1q <mim1q.dev@gmail.com>
2022-09-24 21:55:35 +0200

    Added hanging model to big chains when there's air below them

--- src/main/java/com/github/mim1q/minecells/block/BigChainBlock.java
+++ src/main/java/com/github/mim1q/minecells/block/BigChainBlock.java
@@ -1,16 +1,57 @@
 package com.github.mim1q.minecells.block;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ChainBlock;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.WorldAccess;
+import org.jetbrains.annotations.Nullable;
 
 public class BigChainBlock extends ChainBlock {
 
+  public static BooleanProperty HANGING = BooleanProperty.of("hanging");
+
   public BigChainBlock(Settings settings) {
     super(settings);
+    setDefaultState(getDefaultState().with(HANGING, false));
+  }
+
+
+  @Override
+  public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+    return getHangingState(super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos), world, pos);
+  }
+
+  protected BlockState getHangingState(BlockState state, WorldAccess world, BlockPos pos) {
+    BlockState stateBelow = world.getBlockState(pos.down());
+    boolean chainBelow = stateBelow.getBlock() instanceof BigChainBlock && stateBelow.get(AXIS) == Direction.Axis.Y;
+    boolean vertical = state.get(AXIS) == Direction.Axis.Y;
+    boolean solidBelow = stateBelow.isSideSolidFullSquare(world, pos.down(), Direction.UP);
+    boolean hanging = vertical && !(chainBelow || solidBelow);
+    return state.with(HANGING, hanging);
+  }
+
+  @Nullable
+  @Override
+  public BlockState getPlacementState(ItemPlacementContext ctx) {
+    BlockState state = super.getPlacementState(ctx);
+    if (state == null) {
+      return null;
+    }
+    return getHangingState(state, ctx.getWorld(), ctx.getBlockPos());
+  }
+
+  @Override
+  protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+    super.appendProperties(builder);
+    builder.add(HANGING);
   }
 
   public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
