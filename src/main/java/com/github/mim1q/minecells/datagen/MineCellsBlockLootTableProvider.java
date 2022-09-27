a17b309ac65f30504f95c07cdf9bb8712c2ca5c6
mim1q <mim1q.dev@gmail.com>
2022-09-27 21:15:00 +0200

    Added helper functions

--- src/main/java/com/github/mim1q/minecells/datagen/MineCellsBlockLootTableProvider.java
+++ src/main/java/com/github/mim1q/minecells/datagen/MineCellsBlockLootTableProvider.java
@@ -52,69 +52,77 @@ public class MineCellsBlockLootTableProvider extends SimpleFabricLootTableProvid
     biConsumer.accept(
       MineCellsBlocks.HANGED_SKELETON.getLootTableId(),
       LootTable.builder()
-        .pool(LootPool.builder().rolls(ConstantLootNumberProvider.create(1)).with(
-          ItemEntry.builder(Items.BONE).apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(2, 5)))
-        ))
-        .pool(LootPool.builder().rolls(ConstantLootNumberProvider.create(1)).with(
-          ItemEntry.builder(Items.SKELETON_SKULL).apply(SetCountLootFunction.builder(ConstantLootNumberProvider.create(1)))
-        ))
+        .pool(simplePool(ItemEntry.builder(Items.BONE), 2, 5))
+        .pool(simplePool(ItemEntry.builder(Items.SKELETON_SKULL), 1))
     );
 
     biConsumer.accept(
       MineCellsBlocks.CHAIN_PILE_BLOCK.getLootTableId(),
       LootTable.builder()
-        .pool(LootPool.builder().rolls(ConstantLootNumberProvider.create(1)).with(
-          ItemEntry.builder(MineCellsBlocks.CHAIN_PILE_BLOCK)
-            .conditionally(MatchToolLootCondition.builder(ItemPredicate.Builder.create().enchantment(new EnchantmentPredicate(Enchantments.SILK_TOUCH, NumberRange.IntRange.atLeast(1)))))
-        ))
-        .pool(LootPool.builder().rolls(ConstantLootNumberProvider.create(1)).with(
-          ItemEntry.builder(Blocks.CHAIN)
-            .conditionally(MatchToolLootCondition.builder(ItemPredicate.Builder.create().enchantment(new EnchantmentPredicate(Enchantments.SILK_TOUCH, NumberRange.IntRange.atLeast(1)))).invert())
-            .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1, 2)))
-        ))
-        .pool(LootPool.builder().rolls(ConstantLootNumberProvider.create(1)).with(
-          ItemEntry.builder(MineCellsBlocks.BIG_CHAIN)
-            .conditionally(MatchToolLootCondition.builder(ItemPredicate.Builder.create().enchantment(new EnchantmentPredicate(Enchantments.SILK_TOUCH, NumberRange.IntRange.atLeast(1)))).invert())
-            .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1, 2)))
-        ))
+        .pool(simplePool(silkTouchEntry(MineCellsBlocks.CHAIN_PILE_BLOCK), 1))
+        .pool(simplePool(noSilkTouchEntry(MineCellsBlocks.BIG_CHAIN), 1, 2))
+        .pool(simplePool(noSilkTouchEntry(Blocks.CHAIN), 1, 2))
     );
 
     biConsumer.accept(
       MineCellsBlocks.CHAIN_PILE.getLootTableId(),
       LootTable.builder()
-        .pool(LootPool.builder().rolls(ConstantLootNumberProvider.create(1)).with(
-          ItemEntry.builder(MineCellsBlocks.CHAIN_PILE)
-            .conditionally(MatchToolLootCondition.builder(
-              ItemPredicate.Builder.create().enchantment(new EnchantmentPredicate(Enchantments.SILK_TOUCH, NumberRange.IntRange.ANY))
-            ))
-            .apply(SetCountLootFunction.builder(ConstantLootNumberProvider.create(1)))
-            .alternatively(
-              ItemEntry.builder(MineCellsBlocks.BIG_CHAIN)
-                .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1, 3)))
-            )
-        ))
+        .pool(simplePool(silkTouchEntry(MineCellsBlocks.CHAIN_PILE), 1))
+        .pool(simplePool(noSilkTouchEntry(MineCellsBlocks.BIG_CHAIN), 2, 3))
     );
 
     biConsumer.accept(
       MineCellsBlocks.CAGE.getLootTableId(),
-      LootTable.builder()
-        .pool(LootPool.builder().rolls(ConstantLootNumberProvider.create(1)).with(
+      LootTable.builder().pool(
+        LootPool.builder().rolls(ConstantLootNumberProvider.create(1)).with(
           ItemEntry.builder(MineCellsItems.BROKEN_CAGE)
-            .conditionally(
-              new BlockStatePropertyLootCondition.Builder(MineCellsBlocks.CAGE)
-                .properties(StatePredicate.Builder.create().exactMatch(CageBlock.BROKEN, true))
-            )
+            .conditionally(new BlockStatePropertyLootCondition.Builder(MineCellsBlocks.CAGE)
+            .properties(StatePredicate.Builder.create().exactMatch(CageBlock.BROKEN, true)))
             .apply(SetCountLootFunction.builder(ConstantLootNumberProvider.create(1)))
             .alternatively(ItemEntry.builder(MineCellsItems.CAGE))
-        ))
+        )
+      )
     );
   }
 
-  public void generateSelfDroppingBlock(BiConsumer<Identifier, LootTable.Builder> biConsumer, Block block) {
+  public static void generateSelfDroppingBlock(BiConsumer<Identifier, LootTable.Builder> biConsumer, Block block) {
     biConsumer.accept(block.getLootTableId(), FabricBlockLootTableProvider.drops(block));
   }
 
-  public void generateBlock(BiConsumer<Identifier, LootTable.Builder> biConsumer, Block block, ItemConvertible drop) {
+  public static void generateBlock(BiConsumer<Identifier, LootTable.Builder> biConsumer, Block block, ItemConvertible drop) {
     biConsumer.accept(block.getLootTableId(), FabricBlockLootTableProvider.drops(drop));
+  }
+
+  public static LootPool.Builder simplePool(LeafEntry.Builder<?> entryBuilder, int count) {
+    return LootPool.builder().rolls(ConstantLootNumberProvider.create(1)).with(
+      entryBuilder.apply(
+        SetCountLootFunction.builder(ConstantLootNumberProvider.create(count))
+      )
+    );
+  }
+
+  public static LootPool.Builder simplePool(LeafEntry.Builder<?> entryBuilder, int min, int max) {
+    return LootPool.builder().rolls(ConstantLootNumberProvider.create(1)).with(
+      entryBuilder.apply(
+        SetCountLootFunction.builder(UniformLootNumberProvider.create(min, max))
+      )
+    );
+  }
+
+  public static LeafEntry.Builder<?> silkTouchEntry(ItemConvertible item, boolean silkTouch) {
+    var condition = MatchToolLootCondition.builder(
+      ItemPredicate.Builder.create().enchantment(
+        new EnchantmentPredicate(Enchantments.SILK_TOUCH, NumberRange.IntRange.atLeast(1))
+      )
+    );
+    return ItemEntry.builder(item).conditionally(silkTouch ? condition : condition.invert());
+  }
+
+  public static LeafEntry.Builder<?> silkTouchEntry(ItemConvertible item) {
+    return silkTouchEntry(item, true);
+  }
+
+  public static LeafEntry.Builder<?> noSilkTouchEntry(ItemConvertible item) {
+    return silkTouchEntry(item, false);
   }
 }
