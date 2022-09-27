3656fa39fb64a9e564ee330d014ceb6156ac8fab
mim1q <mim1q.dev@gmail.com>
2022-09-27 22:05:36 +0200

    Updated hanged skeleton block

--- src/main/java/com/github/mim1q/minecells/block/SkeletonDecorationBlock.java
+++ src/main/java/com/github/mim1q/minecells/block/SkeletonDecorationBlock.java
@@ -1,9 +1,7 @@
 package com.github.mim1q.minecells.block;
 
 import com.github.mim1q.minecells.util.ModelUtils;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.ShapeContext;
+import net.minecraft.block.*;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.DirectionProperty;
@@ -13,13 +11,17 @@ import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.WorldAccess;
 import org.jetbrains.annotations.Nullable;
 
 public class SkeletonDecorationBlock extends Block {
 
   public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
 
-  public static final VoxelShape SHAPE = VoxelShapes.cuboid(0.125D, 0.25D, 0.375D, 0.875D, 1.0D, 0.625D);
+  public static final VoxelShape SHAPE = VoxelShapes.union(
+    VoxelShapes.cuboid(0.125D, 0.25D, 0.4375D, 0.875D, 1.0D, 0.5625D),
+    VoxelShapes.cuboid(0.25D, 0.25D, 0.375D, 0.75D, 1.0D, 0.625D)
+  );
 
   public SkeletonDecorationBlock(Settings settings) {
     super(settings);
@@ -39,6 +41,18 @@ public class SkeletonDecorationBlock extends Block {
   @Override
   public BlockState getPlacementState(ItemPlacementContext ctx) {
     return getDefaultState().with(FACING, ctx.getPlayerFacing());
+  }
+
+  @Override
+  @SuppressWarnings("deprecation")
+  public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+    if (neighborPos == pos.up()) {
+      if (neighborState.getBlock() == Blocks.CHAIN && neighborState.get(ChainBlock.AXIS) == Direction.Axis.Y) {
+        return state;
+      }
+      return Blocks.AIR.getDefaultState();
+    }
+    return state;
   }
 
   @Override
