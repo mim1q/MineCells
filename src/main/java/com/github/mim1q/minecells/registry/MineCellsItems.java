f0dffdfb764c0c8b7b76361d35accea6808cb340
mim1q <mim1q.dev@gmail.com>
2022-09-30 16:12:45 +0200

    updated item registering

--- src/main/java/com/github/mim1q/minecells/registry/MineCellsItems.java
+++ src/main/java/com/github/mim1q/minecells/registry/MineCellsItems.java
@@ -14,25 +14,55 @@ public class MineCellsItems {
 
   private static final Set<Item> simpleItems = new HashSet<>();
 
-  public static final Item ELEVATOR_MECHANISM = new Item(new FabricItemSettings().group(MineCellsItemGroups.MINECELLS_BLOCKS_AND_ITEMS));
-  public static final Item INTERDIMENSIONAL_RUNE = new InterdimensionalRuneItem(new Item.Settings().group(MineCellsItemGroups.MINECELLS_BLOCKS_AND_ITEMS).maxCount(1).maxDamage(20));
-  public static final Item CHARGED_INTERDIMENSIONAL_RUNE = new Item(new FabricItemSettings().group(MineCellsItemGroups.MINECELLS_BLOCKS_AND_ITEMS).maxCount(1));
-  public static final Item BIOME_BANNER = new AliasedBlockItem(MineCellsBlocks.BIOME_BANNER, new FabricItemSettings().group(MineCellsItemGroups.MINECELLS_BLOCKS_AND_ITEMS));
-  public static final BucketItem SEWAGE_BUCKET = new BucketItem(MineCellsFluids.STILL_SEWAGE, new Item.Settings().recipeRemainder(Items.BUCKET).maxCount(1));
-  public static final BucketItem ANCIENT_SEWAGE_BUCKET = new BucketItem(MineCellsFluids.STILL_ANCIENT_SEWAGE, new Item.Settings().recipeRemainder(Items.BUCKET).maxCount(1));
+  public static final Item ELEVATOR_MECHANISM = registerSimpleItem(
+    new Item(new FabricItemSettings().group(MineCellsItemGroups.MINECELLS_BLOCKS_AND_ITEMS)),
+    "elevator_mechanism"
+  );
 
-  public static final AssassinsDaggerItem ASSASSINS_DAGGER = new AssassinsDaggerItem(new FabricItemSettings()
-    .group(MineCellsItemGroups.MINECELLS_WEAPONS)
-    .maxDamage(1200));
+  public static final Item INTERDIMENSIONAL_RUNE = registerSimpleItem(
+    new InterdimensionalRuneItem(new FabricItemSettings()
+      .group(MineCellsItemGroups.MINECELLS_BLOCKS_AND_ITEMS)
+      .maxCount(1)
+    ),
+    "interdimensional_rune"
+  );
+
+  public static final Item CHARGED_INTERDIMENSIONAL_RUNE = registerSimpleItem(
+    new Item(new FabricItemSettings().group(MineCellsItemGroups.MINECELLS_BLOCKS_AND_ITEMS).maxCount(1)),
+    "charged_interdimensional_rune"
+  );
+
+  public static final Item BIOME_BANNER = registerSimpleItem(
+    new AliasedBlockItem(MineCellsBlocks.BIOME_BANNER, new FabricItemSettings()
+      .group(MineCellsItemGroups.MINECELLS_BLOCKS_AND_ITEMS)),
+    "biome_banner"
+  );
+
+  public static final Item SEWAGE_BUCKET = registerSimpleItem(
+    new BucketItem(MineCellsFluids.STILL_SEWAGE, new FabricItemSettings()
+      .maxCount(1)
+      .recipeRemainder(Items.BUCKET)
+      .group(MineCellsItemGroups.MINECELLS_BLOCKS_AND_ITEMS)
+    ),
+    "sewage_bucket"
+  );
+
+  public static final Item ANCIENT_SEWAGE_BUCKET = registerSimpleItem(
+    new BucketItem(MineCellsFluids.STILL_ANCIENT_SEWAGE, new FabricItemSettings()
+      .maxCount(1)
+      .recipeRemainder(Items.BUCKET)
+      .group(MineCellsItemGroups.MINECELLS_BLOCKS_AND_ITEMS)
+    ),
+    "ancient_sewage_bucket"
+  );
+
+  public static final Item ASSASSINS_DAGGER = new AssassinsDaggerItem(new FabricItemSettings()
+      .maxCount(1)
+      .maxDamage(1200)
+      .group(MineCellsItemGroups.MINECELLS_WEAPONS)
+  );
 
   public static void init() {
-    registerSimpleItem(ELEVATOR_MECHANISM, "elevator_mechanism");
-    registerSimpleItem(SEWAGE_BUCKET, "sewage_bucket");
-    registerSimpleItem(ANCIENT_SEWAGE_BUCKET, "ancient_sewage_bucket");
-    registerSimpleItem(INTERDIMENSIONAL_RUNE, "interdimensional_rune");
-    registerSimpleItem(CHARGED_INTERDIMENSIONAL_RUNE, "charged_interdimensional_rune");
-    registerSimpleItem(BIOME_BANNER, "biome_banner");
-
     Registry.register(Registry.ITEM, MineCells.createId("assassins_dagger"), ASSASSINS_DAGGER);
   }
 
