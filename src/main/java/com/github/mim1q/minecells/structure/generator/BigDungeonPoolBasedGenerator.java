3069bbb23951e91c6cd1386652e5f15fee04eeda
mim1q <mim1q.dev@gmail.com>
2022-10-07 14:33:12 +0200

    refactoring part 1

--- src/main/java/com/github/mim1q/minecells/structure/generator/BigDungeonPoolBasedGenerator.java
+++ src/main/java/com/github/mim1q/minecells/structure/generator/BigDungeonPoolBasedGenerator.java
@@ -4,8 +4,10 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Queues;
 import com.mojang.logging.LogUtils;
 import net.minecraft.block.JigsawBlock;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.structure.*;
+import net.minecraft.structure.JigsawJunction;
+import net.minecraft.structure.PoolStructurePiece;
+import net.minecraft.structure.StructureTemplate;
+import net.minecraft.structure.StructureTemplateManager;
 import net.minecraft.structure.pool.*;
 import net.minecraft.util.BlockRotation;
 import net.minecraft.util.Identifier;
@@ -20,7 +22,6 @@ import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.HeightLimitView;
 import net.minecraft.world.Heightmap;
-import net.minecraft.world.gen.StructureAccessor;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.gen.noise.NoiseConfig;
 import net.minecraft.world.gen.structure.Structure;
@@ -52,7 +53,7 @@ public class BigDungeonPoolBasedGenerator extends StructurePoolBasedGenerator {
         Identifier identifier = id.get();
         Optional<BlockPos> optional = findStartingJigsawPos(structurePoolElement, identifier, pos, blockRotation, structureTemplateManager, chunkRandom);
         if (optional.isEmpty()) {
-          LOGGER.error("No starting jigsaw {} found in start pool {}", identifier, structurePool.getKey().get().getValue());
+          LOGGER.error("No starting jigsaw {} found in start pool {}", identifier, structurePool.getKey().orElse(StructurePools.EMPTY).getValue());
           return Optional.empty();
         }
 
@@ -68,11 +69,7 @@ public class BigDungeonPoolBasedGenerator extends StructurePoolBasedGenerator {
       int i = (blockBox.getMaxX() + blockBox.getMinX()) / 2;
       int j = (blockBox.getMaxZ() + blockBox.getMinZ()) / 2;
       int k;
-      if (projectStartToHeightmap.isPresent()) {
-        k = pos.getY() + chunkGenerator.getHeightOnGround(i, j, projectStartToHeightmap.get(), heightLimitView, context.noiseConfig());
-      } else {
-        k = blockPos2.getY();
-      }
+      k = projectStartToHeightmap.map(type -> pos.getY() + chunkGenerator.getHeightOnGround(i, j, type, heightLimitView, context.noiseConfig())).orElseGet(blockPos2::getY);
 
       int l = blockBox.getMinY() + poolStructurePiece.getGroundLevelDelta();
       poolStructurePiece.translate(0, k - l, 0);
@@ -94,10 +91,8 @@ public class BigDungeonPoolBasedGenerator extends StructurePoolBasedGenerator {
   private static Optional<BlockPos> findStartingJigsawPos(StructurePoolElement pool, Identifier id, BlockPos pos, BlockRotation rotation, StructureTemplateManager structureManager, ChunkRandom random) {
     List<StructureTemplate.StructureBlockInfo> list = pool.getStructureBlockInfos(structureManager, pos, rotation, random);
     Optional<BlockPos> optional = Optional.empty();
-    Iterator var8 = list.iterator();
 
-    while(var8.hasNext()) {
-      StructureTemplate.StructureBlockInfo structureBlockInfo = (StructureTemplate.StructureBlockInfo)var8.next();
+    for (StructureTemplate.StructureBlockInfo structureBlockInfo : list) {
       Identifier identifier = Identifier.tryParse(structureBlockInfo.nbt.getString("name"));
       if (id.equals(identifier)) {
         optional = Optional.of(structureBlockInfo.pos);
@@ -110,7 +105,7 @@ public class BigDungeonPoolBasedGenerator extends StructurePoolBasedGenerator {
 
   private static void generate(NoiseConfig noiseConfig, int maxSize, boolean modifyBoundingBox, ChunkGenerator chunkGenerator, StructureTemplateManager structureTemplateManager, HeightLimitView heightLimitView, Random random, Registry<StructurePool> structurePoolRegistry, PoolStructurePiece firstPiece, List<PoolStructurePiece> pieces, VoxelShape pieceShape) {
     StructurePoolGenerator structurePoolGenerator = new StructurePoolGenerator(structurePoolRegistry, maxSize, chunkGenerator, structureTemplateManager, pieces, random);
-    structurePoolGenerator.structurePieces.addLast(new ShapedPoolStructurePiece(firstPiece, new MutableObject(pieceShape), 0));
+    structurePoolGenerator.structurePieces.addLast(new ShapedPoolStructurePiece(firstPiece, new MutableObject<>(pieceShape), 0));
 
     while(!structurePoolGenerator.structurePieces.isEmpty()) {
       ShapedPoolStructurePiece shapedPoolStructurePiece = structurePoolGenerator.structurePieces.removeFirst();
@@ -119,32 +114,7 @@ public class BigDungeonPoolBasedGenerator extends StructurePoolBasedGenerator {
 
   }
 
-  public static boolean generate(ServerWorld world, RegistryEntry<StructurePool> structurePool, Identifier id, int i, BlockPos pos, boolean keepJigsaws) {
-    ChunkGenerator chunkGenerator = world.getChunkManager().getChunkGenerator();
-    StructureTemplateManager structureTemplateManager = world.getStructureTemplateManager();
-    StructureAccessor structureAccessor = world.getStructureAccessor();
-    Random random = world.getRandom();
-    Structure.Context context = new Structure.Context(world.getRegistryManager(), chunkGenerator, chunkGenerator.getBiomeSource(), world.getChunkManager().getNoiseConfig(), structureTemplateManager, world.getSeed(), new ChunkPos(pos), world, (registryEntry) -> {
-      return true;
-    });
-    Optional<Structure.StructurePosition> optional = generate(context, structurePool, Optional.of(id), i, pos, false, Optional.empty(), 128);
-    if (optional.isPresent()) {
-      StructurePiecesCollector structurePiecesCollector = optional.get().generate();
-      Iterator var13 = structurePiecesCollector.toList().pieces().iterator();
-
-      while(var13.hasNext()) {
-        StructurePiece structurePiece = (StructurePiece)var13.next();
-        if (structurePiece instanceof PoolStructurePiece poolStructurePiece) {
-          poolStructurePiece.generate(world, structureAccessor, chunkGenerator, random, BlockBox.infinite(), pos, keepJigsaws);
-        }
-      }
-
-      return true;
-    } else {
-      return false;
-    }
-  }
-
+  @SuppressWarnings("deprecation")
   static final class StructurePoolGenerator {
     private final Registry<StructurePool> registry;
     private final int maxSize;
@@ -169,14 +139,14 @@ public class BigDungeonPoolBasedGenerator extends StructurePoolBasedGenerator {
       BlockRotation blockRotation = piece.getRotation();
       StructurePool.Projection projection = structurePoolElement.getProjection();
       boolean bl = projection == StructurePool.Projection.RIGID;
-      MutableObject mutableObject = new MutableObject<>();
+      MutableObject<VoxelShape> mutableObject = new MutableObject<>();
       BlockBox blockBox = piece.getBoundingBox();
       int i = blockBox.getMinY();
-      Iterator var15 = structurePoolElement.getStructureBlockInfos(this.structureTemplateManager, blockPos, blockRotation, this.random).iterator();
+      Iterator<StructureTemplate.StructureBlockInfo> var15 = structurePoolElement.getStructureBlockInfos(this.structureTemplateManager, blockPos, blockRotation, this.random).iterator();
 
       label93:
       while(var15.hasNext()) {
-        StructureTemplate.StructureBlockInfo structureBlockInfo = (StructureTemplate.StructureBlockInfo)var15.next();
+        StructureTemplate.StructureBlockInfo structureBlockInfo = var15.next();
         Direction direction = JigsawBlock.getFacing(structureBlockInfo.state);
         BlockPos blockPos2 = structureBlockInfo.pos;
         BlockPos blockPos3 = blockPos2.offset(direction);
@@ -189,7 +159,7 @@ public class BigDungeonPoolBasedGenerator extends StructurePoolBasedGenerator {
           Optional<StructurePool> optional2 = this.registry.getOrEmpty(identifier2);
           if (optional2.isPresent() && (optional2.get().getElementCount() != 0 || Objects.equals(identifier2, StructurePools.EMPTY.getValue()))) {
             boolean bl2 = blockBox.contains(blockPos3);
-            MutableObject mutableObject2;
+            MutableObject<VoxelShape> mutableObject2;
             if (bl2) {
               mutableObject2 = mutableObject;
               if (mutableObject.getValue() == null) {
@@ -205,19 +175,17 @@ public class BigDungeonPoolBasedGenerator extends StructurePoolBasedGenerator {
             }
 
             list.addAll(optional2.get().getElementIndicesInRandomOrder(this.random));
-            Iterator var29 = list.iterator();
 
-            while(var29.hasNext()) {
-              StructurePoolElement structurePoolElement2 = (StructurePoolElement)var29.next();
+            for (StructurePoolElement structurePoolElement2 : list) {
               if (structurePoolElement2 == EmptyPoolElement.INSTANCE) {
                 break;
               }
 
-              Iterator var31 = BlockRotation.randomRotationOrder(this.random).iterator();
+              Iterator<BlockRotation> var31 = BlockRotation.randomRotationOrder(this.random).iterator();
 
               label133:
-              while(var31.hasNext()) {
-                BlockRotation blockRotation2 = (BlockRotation)var31.next();
+              while (var31.hasNext()) {
+                BlockRotation blockRotation2 = var31.next();
                 List<StructureTemplate.StructureBlockInfo> list2 = structurePoolElement2.getStructureBlockInfos(this.structureTemplateManager, BlockPos.ORIGIN, blockRotation2, this.random);
                 BlockBox blockBox2 = structurePoolElement2.getBoundingBox(this.structureTemplateManager, BlockPos.ORIGIN, blockRotation2);
                 int l;
@@ -228,15 +196,9 @@ public class BigDungeonPoolBasedGenerator extends StructurePoolBasedGenerator {
                     } else {
                       Identifier identifier1 = new Identifier(structureBlockInfox.nbt.getString("pool"));
                       Optional<StructurePool> optional3 = this.registry.getOrEmpty(identifier1);
-                      Optional<StructurePool> optional4 = optional3.flatMap((pool) -> {
-                        return this.registry.getOrEmpty(pool.getTerminatorsId());
-                      });
-                      int ii = optional3.map((pool) -> {
-                        return pool.getHighestY(this.structureTemplateManager);
-                      }).orElse(0);
-                      int jj = optional4.map((pool) -> {
-                        return pool.getHighestY(this.structureTemplateManager);
-                      }).orElse(0);
+                      Optional<StructurePool> optional4 = optional3.flatMap((pool) -> this.registry.getOrEmpty(pool.getTerminatorsId()));
+                      int ii = optional3.map((pool) -> pool.getHighestY(this.structureTemplateManager)).orElse(0);
+                      int jj = optional4.map((pool) -> pool.getHighestY(this.structureTemplateManager)).orElse(0);
                       return Math.max(ii, jj);
                     }
                   }).max().orElse(0);
@@ -244,7 +206,7 @@ public class BigDungeonPoolBasedGenerator extends StructurePoolBasedGenerator {
                   l = 0;
                 }
 
-                Iterator var36 = list2.iterator();
+                Iterator<StructureTemplate.StructureBlockInfo> var36 = list2.iterator();
 
                 StructurePool.Projection projection2;
                 boolean bl3;
@@ -261,8 +223,8 @@ public class BigDungeonPoolBasedGenerator extends StructurePoolBasedGenerator {
                       continue label133;
                     }
 
-                    structureBlockInfo2 = (StructureTemplate.StructureBlockInfo)var36.next();
-                  } while(!JigsawBlock.attachmentMatches(structureBlockInfo, structureBlockInfo2));
+                    structureBlockInfo2 = var36.next();
+                  } while (!JigsawBlock.attachmentMatches(structureBlockInfo, structureBlockInfo2));
 
                   BlockPos blockPos4 = structureBlockInfo2.pos;
                   BlockPos blockPos5 = blockPos3.subtract(blockPos4);
@@ -289,9 +251,9 @@ public class BigDungeonPoolBasedGenerator extends StructurePoolBasedGenerator {
                     r = Math.max(l + 1, blockBox4.getMaxY() - blockBox4.getMinY());
                     blockBox4.encompass(new BlockPos(blockBox4.getMinX(), blockBox4.getMinY() + r, blockBox4.getMinZ()));
                   }
-                } while(VoxelShapes.matchesAnywhere((VoxelShape)mutableObject2.getValue(), VoxelShapes.cuboid(Box.from(blockBox4).contract(0.25)), BooleanBiFunction.ONLY_SECOND));
+                } while (VoxelShapes.matchesAnywhere(mutableObject2.getValue(), VoxelShapes.cuboid(Box.from(blockBox4).contract(0.25)), BooleanBiFunction.ONLY_SECOND));
 
-                mutableObject2.setValue(VoxelShapes.combine((VoxelShape)mutableObject2.getValue(), VoxelShapes.cuboid(Box.from(blockBox4)), BooleanBiFunction.ONLY_FIRST));
+                mutableObject2.setValue(VoxelShapes.combine(mutableObject2.getValue(), VoxelShapes.cuboid(Box.from(blockBox4)), BooleanBiFunction.ONLY_FIRST));
                 r = piece.getGroundLevelDelta();
                 int s;
                 if (bl3) {
@@ -330,20 +292,12 @@ public class BigDungeonPoolBasedGenerator extends StructurePoolBasedGenerator {
           LOGGER.warn("Empty or non-existent pool: {}", identifier);
         }
       }
-
-      return;
     }
   }
 
-  private static final class ShapedPoolStructurePiece {
-    final PoolStructurePiece piece;
-    final MutableObject<VoxelShape> pieceShape;
-    final int currentSize;
-
-    ShapedPoolStructurePiece(PoolStructurePiece piece, MutableObject<VoxelShape> pieceShape, int currentSize) {
-      this.piece = piece;
-      this.pieceShape = pieceShape;
-      this.currentSize = currentSize;
-    }
-  }
+  private record ShapedPoolStructurePiece(
+    PoolStructurePiece piece,
+    MutableObject<VoxelShape> pieceShape,
+    int currentSize
+  ) { }
 }
