40c0f2d7e05a0e52bdea723f0e55bb10ec360b08
mim1q <mim1q.dev@gmail.com>
2022-10-12 20:54:04 +0200

    Updated gradle and loom

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,7 +3,7 @@ object Versions {
   // Fabric
   const val fabricLoader = "0.14.9"
   const val fabricApi = "0.61.0+1.19.2"
-  const val yarn = "1.19.2+build.13"
+  const val yarn = "1.19.2+build.20"
   const val loom = "1.0-SNAPSHOT"
   const val omegaConfig = "1.2.3-1.18.1"
 
