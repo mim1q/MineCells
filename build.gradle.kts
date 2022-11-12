7051aef3e8e8c0746353192b390c613d9092da50
mim1q <mim1q.dev@gmail.com>
2022-11-12 15:24:01 +0100

    Automatically set fabric as mod loader

--- build.gradle.kts
+++ build.gradle.kts
@@ -117,6 +117,7 @@ if (secrets.isCurseforgeReady()) {
       id = secrets.curseforgeId
       releaseType = ModData.versionType
       ModData.mcVersions.forEach(::addGameVersion)
+      addGameVersion("Fabric")
       changelog = newChangelog
       changelogType = "markdown"
       relations(closureOf<CurseRelation> {
