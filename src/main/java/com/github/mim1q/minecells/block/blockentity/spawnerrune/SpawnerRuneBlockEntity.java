a13fab2a7595078754daff7f6eeacd7853716a66
mim1q <mim1q.dev@gmail.com>
2022-11-19 23:35:22 +0100

    Spawner checks for nearby entities

--- src/main/java/com/github/mim1q/minecells/block/blockentity/spawnerrune/SpawnerRuneBlockEntity.java
+++ src/main/java/com/github/mim1q/minecells/block/blockentity/spawnerrune/SpawnerRuneBlockEntity.java
@@ -10,7 +10,9 @@ import net.minecraft.entity.SpawnReason;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.ServerWorldAccess;
 import net.minecraft.world.World;
 
@@ -18,7 +20,8 @@ import java.util.List;
 
 public class SpawnerRuneBlockEntity extends BlockEntity {
   private SpawnerRuneData data = new SpawnerRuneData("", new EntryList(), 0, 0, 0, 0, 0);
-  private int cooldown = 0;
+  private int cooldown = 20;
+  private boolean canCooldown = false;
 
   public SpawnerRuneBlockEntity(BlockPos pos, BlockState state) {
     super(MineCellsBlockEntities.SPAWNER_RUNE_BLOCK_ENTITY, pos, state);
@@ -26,24 +29,49 @@ public class SpawnerRuneBlockEntity extends BlockEntity {
 
   public static void tick(World world, BlockPos pos, BlockState state, SpawnerRuneBlockEntity entity) {
     if (world.isClient) return;
-    entity.cooldown = Math.max(0, entity.cooldown - 1);
-    if (entity.cooldown == 0 && world.getTime() % 10 == 0) {
-      if (world.getClosestPlayer(pos.getX(), pos.getY(), pos.getZ(), entity.data.playerRange, false) != null) {
-        entity.cooldown = entity.data.maxCooldown;
-        spawnEntities(world, pos, entity);
+
+    if (entity.canCooldown) {
+      entity.cooldown = Math.max(0, entity.cooldown - 1);
+    }
+
+    if (world.getTime() % 10 == 0) {
+      entity.canCooldown = !entity.areEntitiesNearby();
+      if (entity.cooldown == 0) {
+        if (world.getClosestPlayer(pos.getX(), pos.getY(), pos.getZ(), entity.data.playerRange, false) != null) {
+          entity.cooldown = entity.data.maxCooldown;
+          spawnEntities(world, pos, entity);
+        }
       }
     }
+
+    entity.markDirty();
+  }
+
+  private boolean areEntitiesNearby() {
+    if (cooldown <= 0 || world == null) {
+      return true;
+    }
+    EntryList list = data.entryList;
+    for (int i = 0; i < list.entries.size(); i++) {
+      EntityType<?> type = list.entries.get(i).entityType;
+      float range = (data.spawnRadius + 1.0F) * 2.0F;
+      List<? extends Entity> entities = world.getEntitiesByType(type, Box.of(Vec3d.ofCenter(pos), range, range, range), Entity::isAlive);
+      if (entities.size() > 0) {
+        return true;
+      }
+    }
+    return false;
   }
 
   private static void spawnEntities(World world, BlockPos pos, SpawnerRuneBlockEntity entity) {
     int rolls = entity.data.minRolls + world.random.nextInt(entity.data.maxRolls - entity.data.minRolls + 1);
     List<EntityType<?>> entityTypes = entity.data.entryList.selectEntityTypes(rolls, world.random);
     for (EntityType<?> entityType : entityTypes) {
-      spawnEntity(world, entityType, findPos(world, pos, entity.data.spawnRadius));
+      int id = spawnEntity(world, entityType, findPos(world, pos, entity.data.spawnRadius));
     }
   }
 
-  private static void spawnEntity(World world, EntityType<?> type, BlockPos pos) {
+  private static int spawnEntity(World world, EntityType<?> type, BlockPos pos) {
     Entity spawnedEntity = type.create(world);
     if (spawnedEntity != null) {
       spawnedEntity.refreshPositionAndAngles(pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5, 0, 0);
@@ -51,7 +79,9 @@ public class SpawnerRuneBlockEntity extends BlockEntity {
         mcEntity.initialize((ServerWorldAccess) world, world.getLocalDifficulty(pos), SpawnReason.SPAWNER, null, null);
       }
       world.spawnEntity(spawnedEntity);
+      return spawnedEntity.getId();
     }
+    return -1;
   }
 
   private static BlockPos findPos(World world, BlockPos pos, float radius) {
