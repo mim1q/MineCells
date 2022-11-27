f55770f0e6afa06b0f1116a199a8cb1e7ba18408
mim1q <mim1q.dev@gmail.com>
2022-11-27 12:29:22 +0100

    Created prison bricks blocks

--- src/main/java/com/github/mim1q/minecells/registry/MineCellsBlocks.java
+++ src/main/java/com/github/mim1q/minecells/registry/MineCellsBlocks.java
@@ -32,6 +32,21 @@ public class MineCellsBlocks {
     "hardstone"
   );
 
+  public static final Block PRISON_BRICKS = registerBlockWithItem(
+    new Block(FabricBlockSettings.copyOf(Blocks.STONE_BRICKS)),
+    "prison_bricks"
+  );
+
+  public static final Block PRISON_BRICK_SLAB = registerBlockWithItem(
+    new SlabBlock(FabricBlockSettings.copyOf(Blocks.STONE_BRICK_SLAB)),
+    "prison_brick_slab"
+  );
+
+  public static final Block PRISON_BRICK_STAIRS = registerBlockWithItem(
+    new StairsBlock(PRISON_BRICKS.getDefaultState(), FabricBlockSettings.copyOf(Blocks.STONE_BRICK_STAIRS)),
+    "prison_brick_stairs"
+  );
+
   public static final Block PUTRID_LOG = registerBlockWithItem(
     new PillarBlock(FabricBlockSettings.copyOf(Blocks.OAK_LOG)),
     "putrid_log"
