57416b859a73f28765a5e112bc559345737f1757
mim1q <mim1q.dev@gmail.com>
2022-11-29 20:58:20 +0100

    Stone now requires silk touch

--- pyDatagen/main.py
+++ pyDatagen/main.py
@@ -9,12 +9,17 @@ from template import Template, TemplateType
 def generate_data(output_path: str):
     generator = PresetGenerator("minecells", output_path)
 
+    # Block Sets
     generator.generate_preset(block_set_presets.StoneBlockSet("small_prison_bricks", "small_prison_brick"))
     generator.generate_preset(block_set_presets.StoneBlockSet("prison_bricks", "prison_brick"))
     generator.generate_preset(block_set_presets.StoneBlockSet("prison_stone"))
     generator.generate_preset(block_set_presets.StoneBlockSet("prison_cobblestone"))
     generator.generate_preset(block_set_presets.WoodBlockSet("putrid_planks", "putrid"))
 
+    # Custom Drops
+    generator.generate_template(Template(TemplateType.BLOCK_LOOT_TABLE, "silk_touch_simple_drop", "prison_stone", {"item": "prison_cobblestone", "silk_touch_item": "prison_stone"}))
+
+    # Custom Recipes
     generator.generate_template(Template(TemplateType.RECIPE, "smelting", "prison_cobblestone", {"input": "prison_cobblestone", "output": "prison_stone"}))
     generator.generate_template(Template(TemplateType.RECIPE, "packed_2x2", "prison_bricks", {"input": "prison_stone", "output": "prison_bricks", "count": 4}))
     generator.generate_template(Template(TemplateType.RECIPE, "packed_2x2", "small_prison_bricks", {"input": "prison_bricks", "output": "small_prison_bricks", "count": 4}))
