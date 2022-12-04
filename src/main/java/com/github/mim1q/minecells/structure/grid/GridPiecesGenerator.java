be76a34175ecaa7cb246fc926525c6deac5d6285
mim1q <mim1q.dev@gmail.com>
2022-12-04 16:13:00 +0100

    Side corridors generating!

--- src/main/java/com/github/mim1q/minecells/structure/grid/GridPiecesGenerator.java
+++ src/main/java/com/github/mim1q/minecells/structure/grid/GridPiecesGenerator.java
@@ -12,15 +12,17 @@ public class GridPiecesGenerator {
   public static List<GridPiece> generatePieces(BlockPos startPos, Structure.Context context, int size) {
     List<GridPiece> list = new ArrayList<>();
     list.add(new GridPiece(context, MineCells.createId("prison/spawn"), startPos, BlockRotation.NONE, size));
-    for (int i = 1; i < 6; i++) {
+    for (int i = 1; i <= 10; i++) {
       list.add(new GridPiece(context, MineCells.createId("prison/main_corridor"), startPos.add(0, 0, i * size), BlockRotation.NONE, size));
+      int length = context.random().nextInt(3) + 3;
+      for (int j = 1; j < length; j++) {
+        list.add(new GridPiece(context, MineCells.createId("prison/side_corridor"), startPos.add(j * size, 0, i * size), BlockRotation.COUNTERCLOCKWISE_90, size));
+      }
+      int length2 = context.random().nextInt(3) + 3;
+      for (int j = 1; j < length2; j++) {
+        list.add(new GridPiece(context, MineCells.createId("prison/side_corridor"), startPos.add(-j * size, 0, i * size), BlockRotation.CLOCKWISE_90, size));
+      }
     }
-//    for (int z = 0; z < 4; z++) {
-//      for (int x = 0; x < 4; x++) {
-//        BlockPos pos = new BlockPos(startPos.getX() + x * size, startPos.getY(), startPos.getZ() + z * size);
-//        list.add(new GridPiece(context, MineCells.createId("testpiece"), pos, BlockRotation.random(context.random()), size));
-//      }
-//    }
     return list;
   }
 }
