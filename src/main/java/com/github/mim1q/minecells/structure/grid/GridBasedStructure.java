d3db300c8de855973c767bb3bdd2392a8d6fc533
mim1q <mim1q.dev@gmail.com>
2022-12-04 20:28:28 +0100

    Added support for height providers

--- src/main/java/com/github/mim1q/minecells/structure/grid/GridBasedStructure.java
+++ src/main/java/com/github/mim1q/minecells/structure/grid/GridBasedStructure.java
@@ -1,19 +1,38 @@
 package com.github.mim1q.minecells.structure.grid;
 
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.structure.StructurePiecesCollector;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
+import net.minecraft.world.gen.HeightContext;
+import net.minecraft.world.gen.heightprovider.HeightProvider;
 import net.minecraft.world.gen.structure.Structure;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.BiFunction;
 
 public abstract class GridBasedStructure extends Structure {
-  private final GridPiecesGenerator.RoomGridGenerator generator;
+  public static <T extends GridBasedStructure> Codec<T> createGridBasedStructureCodec(
+    BiFunction<Config, HeightProvider, T> constructor
+  ) {
+    return RecordCodecBuilder.<T>mapCodec((instance ->
+      instance.group(
+        Structure.configCodecBuilder(instance),
+        HeightProvider.CODEC.fieldOf("start_height").forGetter(GridBasedStructure::getHeightProvider)
+      )
+        .apply(instance, constructor)
+    )).codec();
+  }
 
-  protected GridBasedStructure(Config config, GridPiecesGenerator.RoomGridGenerator generator) {
+  private final GridPiecesGenerator.RoomGridGenerator generator;
+  private final HeightProvider heightProvider;
+
+  protected GridBasedStructure(Config config, HeightProvider heightProvider, GridPiecesGenerator.RoomGridGenerator generator) {
     super(config);
     this.generator = generator;
+    this.heightProvider = heightProvider;
   }
 
   @Override
@@ -23,14 +42,17 @@ public abstract class GridBasedStructure extends Structure {
 
   private void addPieces(StructurePiecesCollector collector, Structure.Context context) {
     ChunkPos chunkPos = context.chunkPos();
-    int i = chunkPos.x;
-    int j = chunkPos.z;
-    int k = i * 16;
-    int l = j * 16;
-    BlockPos blockPos = new BlockPos(k, 100, l);
+    int x = chunkPos.x * 16;
+    int z = chunkPos.z * 16;
+    int y = this.heightProvider.get(context.random(), new HeightContext(context.chunkGenerator(), context.world()));
+    BlockPos blockPos = new BlockPos(x, y, z);
     List<GridPiece> pieces = GridPiecesGenerator.generatePieces(blockPos, context, 16, this.generator);
     for (GridPiece piece : pieces) {
       collector.addPiece(piece);
     }
+  }
+
+  public HeightProvider getHeightProvider() {
+    return heightProvider;
   }
 }
