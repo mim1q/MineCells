1095ffc74a4beb2d4d6ee69af115d933a76ed7f7
mim1q <mim1q.dev@gmail.com>
2022-12-04 14:29:02 +0100

    Example pieces for the dungeon

--- src/main/java/com/github/mim1q/minecells/structure/grid/GridPiecesGenerator.java
+++ src/main/java/com/github/mim1q/minecells/structure/grid/GridPiecesGenerator.java
@@ -11,12 +11,16 @@ import java.util.List;
 public class GridPiecesGenerator {
   public static List<GridPiece> generatePieces(BlockPos startPos, Structure.Context context, int size) {
     List<GridPiece> list = new ArrayList<>();
-    for (int z = 0; z < 4; z++) {
-      for (int x = 0; x < 4; x++) {
-        BlockPos pos = new BlockPos(startPos.getX() + x * size, startPos.getY(), startPos.getZ() + z * size);
-        list.add(new GridPiece(context, MineCells.createId("testpiece"), pos, BlockRotation.random(context.random()), size));
-      }
+    list.add(new GridPiece(context, MineCells.createId("prison/spawn"), startPos, BlockRotation.NONE, size));
+    for (int i = 1; i < 6; i++) {
+      list.add(new GridPiece(context, MineCells.createId("prison/main_corridor"), startPos.add(0, 0, i * size), BlockRotation.NONE, size));
     }
+//    for (int z = 0; z < 4; z++) {
+//      for (int x = 0; x < 4; x++) {
+//        BlockPos pos = new BlockPos(startPos.getX() + x * size, startPos.getY(), startPos.getZ() + z * size);
+//        list.add(new GridPiece(context, MineCells.createId("testpiece"), pos, BlockRotation.random(context.random()), size));
+//      }
+//    }
     return list;
   }
 }
