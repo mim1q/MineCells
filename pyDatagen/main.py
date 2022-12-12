99630fd6752f066f17a4303cb78c23650b150cad
mim1q <mim1q.dev@gmail.com>
2022-12-12 08:50:41 +0100

    Updated script to include all of prison's template pools

--- pyDatagen/main.py
+++ pyDatagen/main.py
@@ -9,27 +9,27 @@ from template import Template, TemplateType
 
 def generate_data(output_path: str):
 
-    # PRESETS ==========================================================================================================
+    # TEMPLATES AND PRESETS ============================================================================================
 
-    generator = PresetGenerator("minecells", output_path)
+    gen = PresetGenerator("minecells", output_path)
 
     # Block Sets
-    generator.generate_preset(block_set_presets.StoneBlockSet("small_prison_bricks", "small_prison_brick"))
-    generator.generate_preset(block_set_presets.StoneBlockSet("prison_bricks", "prison_brick"))
-    generator.generate_preset(block_set_presets.StoneBlockSet("prison_stone"))
-    generator.generate_preset(block_set_presets.StoneBlockSet("prison_cobblestone"))
-    generator.generate_preset(block_set_presets.WoodBlockSet("putrid_planks", "putrid"))
+    gen.generate_preset(block_set_presets.StoneBlockSet("small_prison_bricks", "small_prison_brick"))
+    gen.generate_preset(block_set_presets.StoneBlockSet("prison_bricks", "prison_brick"))
+    gen.generate_preset(block_set_presets.StoneBlockSet("prison_stone"))
+    gen.generate_preset(block_set_presets.StoneBlockSet("prison_cobblestone"))
+    gen.generate_preset(block_set_presets.WoodBlockSet("putrid_planks", "putrid"))
 
     # Custom Drops
-    generator.generate_template(Template(TemplateType.BLOCK_LOOT_TABLE, "silk_touch_simple_drop", "prison_stone", {"item": "prison_cobblestone", "silk_touch_item": "prison_stone"}))
+    gen.generate_template(Template(TemplateType.BLOCK_LOOT_TABLE, "silk_touch_simple_drop", "prison_stone", {"item": "prison_cobblestone", "silk_touch_item": "prison_stone"}))
 
     # Custom Recipes
-    generator.generate_template(Template(TemplateType.RECIPE, "smelting", "prison_cobblestone", {"input": "prison_cobblestone", "output": "prison_stone"}))
-    generator.generate_template(Template(TemplateType.RECIPE, "packed_2x2", "prison_bricks", {"input": "prison_stone", "output": "prison_bricks", "count": 4}))
-    generator.generate_template(Template(TemplateType.RECIPE, "packed_2x2", "small_prison_bricks", {"input": "prison_bricks", "output": "small_prison_bricks", "count": 4}))
-    generator.generate_template(Template(TemplateType.RECIPE, "stonecutting", "prison_bricks_stonecutting", {"input": "prison_stone", "output": "prison_bricks", "count": 1}))
-    generator.generate_template(Template(TemplateType.RECIPE, "stonecutting", "small_prison_bricks_stonecutting", {"input": "prison_bricks", "output": "small_prison_bricks", "count": 1}))
-    generator.generate_template(Template(TemplateType.RECIPE, "stonecutting", "small_prison_bricks_stonecutting_from_prison_stone", {"input": "prison_stone", "output": "small_prison_bricks", "count": 1}))
+    gen.generate_template(Template(TemplateType.RECIPE, "smelting", "prison_cobblestone", {"input": "prison_cobblestone", "output": "prison_stone"}))
+    gen.generate_template(Template(TemplateType.RECIPE, "packed_2x2", "prison_bricks", {"input": "prison_stone", "output": "prison_bricks", "count": 4}))
+    gen.generate_template(Template(TemplateType.RECIPE, "packed_2x2", "small_prison_bricks", {"input": "prison_bricks", "output": "small_prison_bricks", "count": 4}))
+    gen.generate_template(Template(TemplateType.RECIPE, "stonecutting", "prison_bricks_stonecutting", {"input": "prison_stone", "output": "prison_bricks", "count": 1}))
+    gen.generate_template(Template(TemplateType.RECIPE, "stonecutting", "small_prison_bricks_stonecutting", {"input": "prison_bricks", "output": "small_prison_bricks", "count": 1}))
+    gen.generate_template(Template(TemplateType.RECIPE, "stonecutting", "small_prison_bricks_stonecutting_from_prison_stone", {"input": "prison_stone", "output": "small_prison_bricks", "count": 1}))
 
     # AUTOLANG =========================================================================================================
 
@@ -37,13 +37,29 @@ def generate_data(output_path: str):
     write_autolang_file(output_path, block_autolang)
 
     # TEMPLATE POOLS ===================================================================================================
+    poolgen = TemplatePoolGenerator(output_path + "\\data\\minecells\\worldgen\\template_pool")
 
     # Prison
-    poolgen = TemplatePoolGenerator(output_path + "\\data\\minecells\\worldgen\\template_pool")
+    poolgen.generate_single("minecells:prison/spawn")
+    poolgen.generate_single("minecells:prison/corridor")
+    poolgen.generate_single("minecells:prison/corridor_end")
+    poolgen.generate_single("minecells:prison/main_corridor")
+    poolgen.generate_single("minecells:prison/main_corridor_end")
+    poolgen.generate_single("minecells:prison/chain_lower")
+    poolgen.generate_single("minecells:prison/chain_upper")
+    poolgen.generate_single("minecells:prison/end")
     poolgen.generate_autoprefixed(
-        "prison/ceiling_decoration",
-        [("broken_cage", 1), ("cage", 1), ("chains_0", 1), ("chains_1", 1), ("chains_2", 1), ("cobwebs_0", 1), ("cobwebs_1", 1), ("leaves_0", 1), ("leaves_1", 1), ("leaves_2", 1)]
+        "minecells:prison/ceiling_decoration",
+        [("minecraft:empty", 2), ("broken_cage", 2), ("cage", 2), ("chains_0", 1), ("chains_1", 1), ("chains_2", 1),
+         ("cobwebs_0", 1), ("cobwebs_1", 1), ("leaves_0", 3), ("leaves_1", 3), ("leaves_2", 3)]
     )
+    poolgen.generate_autoprefixed(
+        "minecells:prison/corridor_decoration",
+        [("bars", 1), ("cobwebs", 1), ("crates_0", 1), ("crates_1", 1), ("crates_2", 1), ("crates_3", 1),
+         ("shelves_0", 1), ("shelves_1", 1)]
+    )
+    poolgen.generate_autoprefixed("minecells:prison/spawn_decoration", [("0", 1), ("1", 1), ("2", 1), ("3", 1), ("4", 1), ("5", 1), ("6", 1)])
+    poolgen.generate_autoprefixed("minecells:prison/main_corridor_doorway", [("0", 1), ("1", 1), ("2", 1), ("3", 1)])
 
 
 def main():
--- pyDatagen/template_pools/template_pool.py
+++ pyDatagen/template_pools/template_pool.py
@@ -21,13 +21,13 @@ TEMPLATE = """{
 """
 
 
-def generate_template_pool(elements: [(str, int)]):
+def generate_template_pool(elements: [(str, int)], name: str):
     result = ""
     for element in elements:
         result += ELEMENT_TEMPLATE.replace("{{name}}", element[0]).replace("{{weight}}", str(element[1]))
 
     result = result[:-2]
-    return TEMPLATE.replace("{{elements}}", result)
+    return TEMPLATE.replace("{{elements}}", result).replace("{{name}}", name)
 
 
 class TemplatePoolGenerator:
@@ -35,15 +35,25 @@ class TemplatePoolGenerator:
         self.output_path = output_path
 
     def generate_prefixed(self, file_name: str, elements: [(str, int)], prefix: str):
-        self.save_to_file(file_name, generate_template_pool([(prefix + "/" + element[0], element[1]) for element in elements]))
+        prefixed_elements = []
+        for e in elements:
+            if ":" in e[0]:
+                prefixed_elements.append(e)
+            else:
+                prefixed_elements.append((prefix + "/" + e[0], e[1]))
+        self.save_to_file(file_name, generate_template_pool(prefixed_elements, file_name))
 
     def generate_autoprefixed(self, file_name: str, elements: [(str, int)]):
         self.generate_prefixed(file_name, elements, file_name)
 
     def generate(self, file_name: str, elements: [(str, int)]):
-        self.save_to_file(file_name, generate_template_pool(elements))
+        self.save_to_file(file_name, generate_template_pool(elements, file_name))
+
+    def generate_single(self, file_name: str):
+        self.generate(file_name, [(file_name, 1)])
 
     def save_to_file(self, file_name: str, template: str):
+        file_name = file_name.split(":")[-1]
         output = os.path.join(self.output_path, file_name + ".json")
         os.makedirs(os.path.dirname(output), exist_ok=True)
         with open(self.output_path + "\\" + file_name + ".json", "w") as file:
