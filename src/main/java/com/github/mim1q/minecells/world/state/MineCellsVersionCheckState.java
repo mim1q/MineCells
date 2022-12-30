1aed8e5f590ab66f669d92c16f2942032d39804f
mim1q <mim1q.dev@gmail.com>
2022-12-30 18:44:39 +0100

    Insufferable crypt bump

--- src/main/java/com/github/mim1q/minecells/world/state/MineCellsVersionCheckState.java
+++ src/main/java/com/github/mim1q/minecells/world/state/MineCellsVersionCheckState.java
@@ -19,7 +19,7 @@ import java.util.List;
 public class MineCellsVersionCheckState extends PersistentState {
   // Modify these when significant changes are made to respective aspects of the mod
   private String prison = "1.3.2";
-  private String insufferableCrypt = "1.3.2";
+  private String insufferableCrypt = "1.3.3";
 
   private MineCellsVersionCheckState(NbtCompound nbt) {
     this.prison = nbt.getString("prison");
