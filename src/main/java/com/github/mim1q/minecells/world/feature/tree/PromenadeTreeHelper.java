6157bc7b9c05ea58e575479c14403a5dea586ac7
mim1q <mim1q.dev@gmail.com>
2023-01-07 16:54:11 +0100

    tweaks

--- src/main/java/com/github/mim1q/minecells/world/feature/tree/PromenadeTreeHelper.java
+++ src/main/java/com/github/mim1q/minecells/world/feature/tree/PromenadeTreeHelper.java
@@ -29,9 +29,9 @@ public interface PromenadeTreeHelper {
       origin = origin.add(offset).up();
       replacer.accept(origin, TRUNK_BLOCK);
     }
-    if (random.nextFloat() < 0.5f) {
-      Block chain = random.nextFloat() < 0.25F ? MineCellsBlocks.BIG_CHAIN : Blocks.CHAIN;
-      int length = 3 + random.nextInt(3);
+    if (random.nextFloat() < 0.5F) {
+      Block chain = random.nextFloat() < 0.33F ? MineCellsBlocks.BIG_CHAIN : Blocks.CHAIN;
+      int length = 3 + random.nextInt(8);
       if (canPlaceChain(world, origin.down(), length + 2)) {
         placeChain(replacer, random, origin.down(), length, chain.getDefaultState());
         if (big) {
