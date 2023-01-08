6e991a66ed87b880f88aadae53d7c66102fc0e6a
mim1q <mim1q.dev@gmail.com>
2023-01-08 21:25:36 +0100

    King Statue is now rotatable

--- src/main/java/com/github/mim1q/minecells/client/render/blockentity/statue/DecorativeStatueBlockEntityRenderer.java
+++ src/main/java/com/github/mim1q/minecells/client/render/blockentity/statue/DecorativeStatueBlockEntityRenderer.java
@@ -1,6 +1,7 @@
 package com.github.mim1q.minecells.client.render.blockentity.statue;
 
 import com.github.mim1q.minecells.MineCells;
+import com.github.mim1q.minecells.block.DecorativeStatueBlock;
 import com.github.mim1q.minecells.block.blockentity.DecorativeStatueBlockEntity;
 import com.github.mim1q.minecells.registry.MineCellsRenderers;
 import net.minecraft.client.render.VertexConsumer;
@@ -10,6 +11,7 @@ import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
 import net.minecraft.client.render.entity.model.EntityModelLoader;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Vec3f;
 
 public class DecorativeStatueBlockEntityRenderer implements BlockEntityRenderer<DecorativeStatueBlockEntity> {
   private static final Identifier TEXTURE = MineCells.createId("textures/blockentity/statue/king.png");
@@ -19,13 +21,15 @@ public class DecorativeStatueBlockEntityRenderer implements BlockEntityRenderer<
     EntityModelLoader loader = ctx.getLayerRenderDispatcher();
     this.model = new KingStatueModel(loader.getModelPart(MineCellsRenderers.KING_STATUE_LAYER));
   }
-  
+
   @Override
   public void render(DecorativeStatueBlockEntity entity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
+    float rotation = entity.getCachedState().get(DecorativeStatueBlock.ROTATION) * 22.5F;
     VertexConsumer vertexConsumer = vertexConsumers.getBuffer(model.getLayer(TEXTURE));
     matrices.push();
     matrices.scale(1.0F, -1.0F, -1.0F);
     matrices.translate(0.5F, -1.5F, -0.5F);
+    matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(180.0F + rotation));
     this.model.render(matrices, vertexConsumer, light, overlay, 1.0F, 1.0F, 1.0F, 1.0F);
     matrices.pop();
   }
