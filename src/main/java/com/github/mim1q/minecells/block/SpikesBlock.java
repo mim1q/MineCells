1a2eb9088c36cad9d62dbd774b9046aae256f390
mim1q <mim1q.dev@gmail.com>
2023-01-22 17:08:34 +0100

    Spikes now rotate

--- src/main/java/com/github/mim1q/minecells/block/SpikesBlock.java
+++ src/main/java/com/github/mim1q/minecells/block/SpikesBlock.java
@@ -14,6 +14,7 @@ import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.util.BlockRotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
@@ -64,6 +65,12 @@ public class SpikesBlock extends Block {
   @Override
   public boolean isTranslucent(BlockState state, BlockView world, BlockPos pos) {
     return true;
+  }
+
+  @Override
+  @SuppressWarnings("deprecation")
+  public BlockState rotate(BlockState state, BlockRotation rotation) {
+    return state.with(FACING, rotation.rotate(state.get(FACING)));
   }
 
   @Override
