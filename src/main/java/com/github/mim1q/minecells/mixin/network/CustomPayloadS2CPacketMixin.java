3555604567c04bf6e771376ab89126eb03de5f4a
mim1q <mim1q.dev@gmail.com>
2023-02-14 15:36:46 +0100

    Hotfixed mixin crashing with Krypton

--- src/main/java/com/github/mim1q/minecells/mixin/network/CustomPayloadS2CPacketMixin.java
+++ src/main/java/com/github/mim1q/minecells/mixin/network/CustomPayloadS2CPacketMixin.java
@@ -1,37 +1,27 @@
 package com.github.mim1q.minecells.mixin.network;
 
-import io.netty.buffer.ByteBuf;
 import net.minecraft.network.Packet;
-import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.listener.ClientPlayPacketListener;
 import net.minecraft.network.packet.s2c.play.CustomPayloadS2CPacket;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(value = CustomPayloadS2CPacket.class, priority = 2275)
 public abstract class CustomPayloadS2CPacketMixin implements Packet<ClientPlayPacketListener> {
-  @Redirect(
+  @ModifyConstant(
     method = "<init>(Lnet/minecraft/util/Identifier;Lnet/minecraft/network/PacketByteBuf;)V",
-    at = @At(value = "INVOKE", target = "Lnet/minecraft/network/PacketByteBuf;writerIndex()I", ordinal = 0)
+    constant = @Constant(intValue = 1048576)
   )
-  private int minecells$initRedirectWriterIndex(PacketByteBuf instance) {
-    return 0;
+  private int minecells$initModifyMaxSize(int constant) {
+    return Math.max(constant, 1073741824);
   }
 
-  @Redirect(
+  @ModifyConstant(
     method = "<init>(Lnet/minecraft/network/PacketByteBuf;)V",
-    at = @At(value = "INVOKE", target = "Lnet/minecraft/network/PacketByteBuf;readableBytes()I", ordinal = 0)
+    constant = @Constant(intValue = 1048576)
   )
-  private int minecells$init2RedirectReadableBytes(PacketByteBuf instance) {
-    return 0;
-  }
-
-  @Redirect(
-    method = "<init>(Lnet/minecraft/network/PacketByteBuf;)V",
-    at = @At(value = "INVOKE", target = "Lnet/minecraft/network/PacketByteBuf;readBytes(I)Lio/netty/buffer/ByteBuf;")
-  )
-  private ByteBuf minecells$init2RedirectReadBytes(PacketByteBuf instance, int length) {
-    return instance.readBytes(instance.readableBytes());
+  private int minecells$init2ModifyMaxSize(int constant) {
+    return Math.max(constant, 1073741824);
   }
 }
