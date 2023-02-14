40e312e3b6bc6a08918505c1a45b7d3a60d02669
mim1q <mim1q.dev@gmail.com>
2023-02-14 16:15:47 +0100

    New side structure!

--- pyDatagen/units/promenade.py
+++ pyDatagen/units/promenade.py
@@ -6,7 +6,7 @@ def generate_template_pools(poolgen: TemplatePoolGenerator):
     poolgen.generate_autoprefixed("minecells:promenade/gallows", [("0", 1), ("1", 2), ("2", 2), ("3", 2)])
     poolgen.generate_single("minecells:promenade/king_statue", projection="terrain_matching")
     poolgen.generate_autoprefixed("minecells:promenade/overground_buildings/main", [("0", 3), ("1", 1)], processors="minecells:promenade")
-    poolgen.generate_autoprefixed("minecells:promenade/overground_buildings/side", [("0", 1)], processors="minecells:promenade")
+    poolgen.generate_autoprefixed("minecells:promenade/overground_buildings/side", [("0", 1), ["1", 1]], processors="minecells:promenade")
     poolgen.generate_autoprefixed("minecells:promenade/overground_buildings/pit", [("0", 1)])
     poolgen.generate_autoprefixed("minecells:promenade/underground_buildings/entry", [("0", 1)], processors="minecells:promenade")
     poolgen.generate_autoprefixed("minecells:promenade/underground_buildings/shaft", [("0", 1)], processors="minecells:promenade")
