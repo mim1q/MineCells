ac86d5b415d09d7c31d6292ee81c196c63844ef9
mim1q <mim1q.dev@gmail.com>
2023-02-16 22:02:57 +0100

    Updated underground generation

--- pyDatagen/units/promenade.py
+++ pyDatagen/units/promenade.py
@@ -12,5 +12,8 @@ def generate_template_pools(poolgen: TemplatePoolGenerator):
     poolgen.generate_autoprefixed("minecells:promenade/underground_buildings/shaft", [("0", 1)], processors="minecells:promenade")
     poolgen.generate_autoprefixed("minecells:promenade/underground_buildings/shaft_bottom", [("0", 1)], processors="minecells:promenade")
     poolgen.generate_indexed("minecells:promenade/underground_buildings/room", [3, 2, 2, 1, 2], processors="minecells:promenade")
+    poolgen.generate_single("minecells:promenade/underground_buildings/room_end_center", processors="minecells:promenade")
+    poolgen.generate_single("minecells:promenade/underground_buildings/room_end_right", processors="minecells:promenade")
+    poolgen.generate_single("minecells:promenade/underground_buildings/room_end_left", processors="minecells:promenade")
     poolgen.generate_autoprefixed("minecells:promenade/doorway", [("0", 2), ("1", 1), ("2", 1), ("3", 1), ("4", 1)], processors="minecells:promenade")
     poolgen.generate_autoprefixed("minecells:promenade/doorway_decoration", [("banners", 1), ("torches", 2), ("minecraft:empty", 4)], processors="minecells:promenade")
--- src/main/java/com/github/mim1q/minecells/structure/grid/generator/PromenadeUndergroundGridGenerator.java
+++ src/main/java/com/github/mim1q/minecells/structure/grid/generator/PromenadeUndergroundGridGenerator.java
@@ -12,16 +12,38 @@ public class PromenadeUndergroundGridGenerator extends PromenadeGridGenerator {
   public static final Identifier SHAFT = MineCells.createId("promenade/underground_buildings/shaft");
   public static final Identifier SHAFT_BOTTOM = MineCells.createId("promenade/underground_buildings/shaft_bottom");
   public static final Identifier ROOM = MineCells.createId("promenade/underground_buildings/room");
+  public static final Identifier END_CENTER = MineCells.createId("promenade/underground_buildings/room_end_center");
+  public static final Identifier END_RIGHT = MineCells.createId("promenade/underground_buildings/room_end_right");
+  public static final Identifier END_LEFT = MineCells.createId("promenade/underground_buildings/room_end_left");
 
   @Override
   protected void addRooms(Random random) {
     super.addRooms(random);
     addRoom(new Vec3i(0, -1, 0), BlockRotation.NONE, SHAFT);
-    addRoom(new Vec3i(0, -2, 0), BlockRotation.NONE, SHAFT_BOTTOM);
-    addRoom(new Vec3i(0, -2, 1), BlockRotation.NONE, ROOM);
-    addRoom(new Vec3i(0, -2, 2), BlockRotation.NONE, ROOM);
-    addRoom(new Vec3i(0, -2, 3), BlockRotation.NONE, ROOM);
-    addRoom(new Vec3i(0, -2, 4), BlockRotation.NONE, ROOM);
+    Vec3i second = addFloor(new Vec3i(0, -2, 0), random);
+    Vec3i third = addFloor(second.down(), random);
+    addFloor(third.down(), random);
+  }
+
+  protected Vec3i addFloor(Vec3i startPos, Random random) {
+    Vec3i origin = new Vec3i(startPos.getX(), startPos.getY(), 0);
+    addRoom(startPos, BlockRotation.NONE, SHAFT_BOTTOM);
+    for (int z = startPos.getZ() + 1; z <= 3; z++) {
+      addRoom(origin.add(0, 0, z), BlockRotation.NONE, ROOM);
+    }
+    addRoom(origin.add(0, 0, 4), BlockRotation.NONE, END_CENTER);
+    addRoom(origin.add(1, 0, 4), BlockRotation.NONE, END_LEFT);
+    addRoom(origin.add(-1, 0, 4), BlockRotation.NONE, END_RIGHT);
+    int leftMinZ = -random.nextInt(4);
+    for (int z = 3; z >= leftMinZ; z--) {
+      addRoom(origin.add(1, 0, z), BlockRotation.NONE, ROOM);
+    }
+    int rightMinZ = -random.nextInt(4);
+    for (int z = 3; z >= rightMinZ; z--) {
+      addRoom(origin.add(-1, 0, z), BlockRotation.NONE, ROOM);
+    }
+    boolean left = random.nextBoolean();
+    return origin.add(left ? 1 : -1, 0, left ? rightMinZ : leftMinZ);
   }
 
   @Override
