a7131ff0826612a8f9c7df295845baca3068bf74
mim1q <mim1q.dev@gmail.com>
2023-02-26 12:23:27 +0100

    Fixed mutated bat's texture stitching issue

--- src/main/java/com/github/mim1q/minecells/client/render/MutatedBatEntityRenderer.java
+++ src/main/java/com/github/mim1q/minecells/client/render/MutatedBatEntityRenderer.java
@@ -4,9 +4,11 @@ import com.github.mim1q.minecells.MineCells;
 import com.github.mim1q.minecells.client.render.model.MutatedBatEntityModel;
 import com.github.mim1q.minecells.entity.MutatedBatEntity;
 import com.github.mim1q.minecells.registry.MineCellsRenderers;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Nullable;
 
 public class MutatedBatEntityRenderer extends MobEntityRenderer<MutatedBatEntity, MutatedBatEntityModel> {
 
@@ -19,5 +21,11 @@ public class MutatedBatEntityRenderer extends MobEntityRenderer<MutatedBatEntity
   @Override
   public Identifier getTexture(MutatedBatEntity entity) {
     return TEXTURE;
+  }
+
+  @Nullable
+  @Override
+  protected RenderLayer getRenderLayer(MutatedBatEntity entity, boolean showBody, boolean translucent, boolean showOutline) {
+    return RenderLayer.getEntityCutout(getTexture(entity));
   }
 }
