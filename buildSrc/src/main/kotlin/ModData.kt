b5e046f7e2ccab4795e475752deae828a3cd693b
mim1q <mim1q.dev@gmail.com>
2023-02-26 13:33:58 +0100

    1.4.0 bump!

--- buildSrc/src/main/kotlin/ModData.kt
+++ buildSrc/src/main/kotlin/ModData.kt
@@ -1,8 +1,8 @@
 object ModData {
   const val group = "com.github.mim1q"
   const val id = "minecells"
-  const val version = "1.3.5"
-  const val versionType = "release"
+  const val version = "1.4.0"
+  const val versionType = "beta"
   val mcVersions = listOf("1.19.2")
   val dependencies = listOf("fabric-api")
 }
