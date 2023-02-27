4014c3f9f4899604d0439e25a54eddb53e0de43f
mim1q <mim1q.dev@gmail.com>
2023-02-27 21:45:04 +0100

    added promenade's torch drop

--- pyDatagen/main.py
+++ pyDatagen/main.py
@@ -51,7 +51,7 @@ def generate_data(output_path: str):
     util.generate_block_items(gen, ["skeleton", "rotting_corpse", "corpse"])
 
     # Custom Drops
-    util.generate_self_dropping_blocks(gen, ["prison_torch", "putrid_boards"])
+    util.generate_self_dropping_blocks(gen, ["prison_torch", "promenade_torch", "putrid_boards"])
     gen.generate_template(Template(TemplateType.BLOCK_LOOT_TABLE, "silk_touch_simple_drop", "prison_stone",
                                    {"item": "prison_cobblestone", "silk_touch_item": "prison_stone"}))
 
