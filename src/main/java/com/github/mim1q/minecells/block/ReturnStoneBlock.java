ac018007d224807d457c7a16278dac0c9f1a845e
mim1q <mim1q.dev@gmail.com>
2023-03-02 19:26:18 +0100

    Added particles

--- src/main/java/com/github/mim1q/minecells/block/ReturnStoneBlock.java
+++ src/main/java/com/github/mim1q/minecells/block/ReturnStoneBlock.java
@@ -1,24 +1,33 @@
 package com.github.mim1q.minecells.block;
 
 import com.github.mim1q.minecells.block.blockentity.ReturnStoneBlockEntity;
+import com.github.mim1q.minecells.particle.colored.ColoredParticle;
+import com.github.mim1q.minecells.registry.MineCellsParticles;
+import com.github.mim1q.minecells.util.ParticleUtils;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.particle.ParticleEffect;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.BlockRotation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 public class ReturnStoneBlock extends Block implements BlockEntityProvider {
   public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
   public static final VoxelShape SHAPE = createCuboidShape(3.0F, 0.0F, 3.0F, 13.0F, 16.0F, 13.0F);
+  public static final ParticleEffect PARTICLE = ColoredParticle.create(MineCellsParticles.SPECKLE, 0xFFC410);
 
   public ReturnStoneBlock(Settings settings) {
     super(settings);
@@ -46,6 +55,16 @@ public class ReturnStoneBlock extends Block implements BlockEntityProvider {
   @SuppressWarnings("deprecation")
   public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
     return SHAPE;
+  }
+
+  @Override
+  public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
+    ParticleUtils.addParticle(
+      (ClientWorld) world,
+      PARTICLE,
+      Vec3d.ofCenter(pos).add(0.0D, 0.75D, 0.0D),
+      Vec3d.fromPolar(random.nextFloat() * 360.0F, random.nextFloat() * 360.0F).multiply(0.03D)
+    );
   }
 
   @Nullable
