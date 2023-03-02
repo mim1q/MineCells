da4eadf4af783c09275c710684037a5318b8332f
mim1q <mim1q.dev@gmail.com>
2023-03-02 20:07:07 +0100

    Return stone functionality added

--- src/main/java/com/github/mim1q/minecells/block/ReturnStoneBlock.java
+++ src/main/java/com/github/mim1q/minecells/block/ReturnStoneBlock.java
@@ -2,6 +2,7 @@ package com.github.mim1q.minecells.block;
 
 import com.github.mim1q.minecells.block.blockentity.ReturnStoneBlockEntity;
 import com.github.mim1q.minecells.particle.colored.ColoredParticle;
+import com.github.mim1q.minecells.registry.MineCellsBlocks;
 import com.github.mim1q.minecells.registry.MineCellsParticles;
 import com.github.mim1q.minecells.util.ParticleUtils;
 import net.minecraft.block.Block;
@@ -10,17 +11,22 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.particle.ParticleEffect;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.Properties;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.BlockRotation;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.Heightmap;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
@@ -37,6 +43,26 @@ public class ReturnStoneBlock extends Block implements BlockEntityProvider {
   protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
     super.appendProperties(builder);
     builder.add(FACING);
+  }
+
+  @Override
+  @SuppressWarnings("deprecation")
+  public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+    if (world.isClient()) {
+      return ActionResult.SUCCESS;
+    }
+    BlockPos topPos;
+    int i = 0;
+    do {
+      if (i > 30) {
+        return ActionResult.FAIL;
+      }
+      topPos = world.getTopPosition(Heightmap.Type.WORLD_SURFACE, pos.west(i));
+      i++;
+    } while (!world.getBlockState(topPos.down()).isOf(MineCellsBlocks.WILTED_GRASS_BLOCK));
+    Vec3d tpPos = Vec3d.ofBottomCenter(topPos);
+    player.teleport(tpPos.x, tpPos.y, tpPos.z);
+    return ActionResult.SUCCESS;
   }
 
   @Nullable
