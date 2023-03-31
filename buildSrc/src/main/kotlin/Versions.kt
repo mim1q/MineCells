3262c602fee677fd0cf76febff0c2e2a150405df
mim1q <mim1q.dev@gmail.com>
2023-03-31 18:51:07 +0200

    Updated loom and mappings

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,8 +3,8 @@ object Versions {
   // Fabric
   const val fabricLoader = "0.14.9"
   const val fabricApi = "0.61.0+1.19.2"
-  const val yarn = "1.19.2+build.20"
-  const val loom = "1.0-SNAPSHOT"
+  const val yarn = "1.19.2+build.28"
+  const val loom = "1.1-SNAPSHOT"
   const val omegaConfig = "1.2.3-1.18.1"
   const val mixinExtras = "0.1.1"
 
