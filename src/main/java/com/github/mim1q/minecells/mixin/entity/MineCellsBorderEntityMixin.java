8253b3e3bcd96a8bb3a7c0fafa936fc3129d287e
mim1q <mim1q.dev@gmail.com>
2023-04-30 18:20:59 +0200

    teleport prevention

--- src/main/java/com/github/mim1q/minecells/mixin/entity/MineCellsBorderEntityMixin.java
+++ src/main/java/com/github/mim1q/minecells/mixin/entity/MineCellsBorderEntityMixin.java
@@ -1,5 +1,6 @@
 package com.github.mim1q.minecells.mixin.entity;
 
+import com.github.mim1q.minecells.accessor.MineCellsBorderEntity;
 import com.github.mim1q.minecells.dimension.MineCellsDimensions;
 import com.github.mim1q.minecells.util.MathUtils;
 import net.minecraft.entity.Entity;
@@ -19,23 +20,35 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 
 @Mixin(Entity.class)
-public abstract class MineCellsBorderEntityMixin implements Nameable, EntityLike, CommandOutput {
+public abstract class MineCellsBorderEntityMixin implements Nameable, EntityLike, CommandOutput, MineCellsBorderEntity {
   @Shadow public int age;
   @Shadow public abstract BlockPos getBlockPos();
   @Shadow public World world;
+  @Shadow public abstract void stopRiding();
 
   private final WorldBorder minecellsBorder = new WorldBorder();
 
+  @Override
+  public WorldBorder getMineCellsBorder() {
+    return minecellsBorder;
+  }
+
   @Inject(method = "tick", at = @At("HEAD"))
   public void minecells$updateBorderOnTick(CallbackInfo ci) {
-    if (age % 20 == 0 && MineCellsDimensions.isMineCellsDimension(world)) {
-      var pos = MathUtils.getClosestMultiplePosition(getBlockPos(), 1024);
-      minecellsBorder.setCenter(pos.getX() + 0.5D, pos.getZ() + 0.5D);
-      minecellsBorder.setSize(1023);
+    if (MineCellsDimensions.isMineCellsDimension(world)) {
+      if (age % 20 == 0) {
+        var pos = MathUtils.getClosestMultiplePosition(getBlockPos(), 1024);
+        minecellsBorder.setCenter(pos.getX() + 0.5D, pos.getZ() + 0.5D);
+        minecellsBorder.setSize(1023);
+      }
+      if (minecellsBorder.getDistanceInsideBorder((Entity)(Object)this) <= 2.0) {
+        stopRiding();
+      }
     }
   }
 
@@ -56,5 +69,12 @@ public abstract class MineCellsBorderEntityMixin implements Nameable, EntityLike
       return ((MineCellsBorderEntityMixin)(Object)entity).minecellsBorder;
     }
     return original;
+  }
+
+  @Inject(method = "startRiding(Lnet/minecraft/entity/Entity;)Z", at = @At("HEAD"), cancellable = true)
+  public void minecells$cancelStartRiding(Entity entity, CallbackInfoReturnable<Boolean> cir) {
+    if (minecellsBorder.getDistanceInsideBorder(entity) < 2.0D) {
+      cir.setReturnValue(false);
+    }
   }
 }
