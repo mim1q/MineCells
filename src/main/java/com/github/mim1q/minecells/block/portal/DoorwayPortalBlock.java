e08616790e95aeda2c9a83fdd066c99de4df97ac
mim1q <mim1q.dev@gmail.com>
2023-05-15 21:44:45 +0200

    Created base for doorway portals

---
+++ src/main/java/com/github/mim1q/minecells/block/portal/DoorwayPortalBlock.java
@@ -0,0 +1,9 @@
+package com.github.mim1q.minecells.block.portal;
+
+import net.minecraft.block.Block;
+
+public class DoorwayPortalBlock extends Block {
+  public DoorwayPortalBlock(Settings settings) {
+    super(settings);
+  }
+}
