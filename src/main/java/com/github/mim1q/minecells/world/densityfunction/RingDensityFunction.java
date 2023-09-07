5fbb057caf7d4005e4b9ff77b4172a9dcbd39928
mim1q <mim1q.dev@gmail.com>
2023-09-07 08:56:34 +0200

    Ramparts terrain experiments

--- src/main/java/com/github/mim1q/minecells/world/densityfunction/RingDensityFunction.java
+++ src/main/java/com/github/mim1q/minecells/world/densityfunction/RingDensityFunction.java
@@ -7,7 +7,17 @@ import net.minecraft.util.dynamic.CodecHolder;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.gen.densityfunction.DensityFunction;
 
-public class RingDensityFunction implements DensityFunction {
+public record RingDensityFunction(
+  int gridSize,
+  int minRadius,
+  int maxRadius,
+  int blendRadius,
+  double fromValue,
+  double toValue,
+  int offsetX,
+  int offsetZ,
+  boolean offsetGrid
+) implements DensityFunction {
   public static final CodecHolder<RingDensityFunction> CODEC_HOLDER = CodecHolder.of(
     RecordCodecBuilder.create(instance -> instance.group(
       Codec.INT.optionalFieldOf("grid_size", 1024).forGetter(it -> it.gridSize),
@@ -15,38 +25,21 @@ public class RingDensityFunction implements DensityFunction {
       Codec.INT.optionalFieldOf("max_radius", 2048).forGetter(it -> it.maxRadius),
       Codec.INT.optionalFieldOf("blend_radius", 16).forGetter(it -> it.blendRadius),
       Codec.DOUBLE.optionalFieldOf("from_value", 0.0).forGetter(it -> it.fromValue),
-      Codec.DOUBLE.optionalFieldOf("to_value", 1.0).forGetter(it -> it.toValue)
+      Codec.DOUBLE.optionalFieldOf("to_value", 1.0).forGetter(it -> it.toValue),
+      Codec.INT.optionalFieldOf("offset_x", 0).forGetter(it -> it.offsetX),
+      Codec.INT.optionalFieldOf("offset_z", 0).forGetter(it -> it.offsetZ),
+      Codec.BOOL.optionalFieldOf("offset_grid", false).forGetter(it -> it.offsetGrid)
     ).apply(instance, RingDensityFunction::new))
   );
 
-  private final int gridSize;
-  private final int minRadius;
-  private final int minRadiusSquared;
-  private final int maxRadius;
-  private final int maxRadiusSquared;
-  private final int blendRadius;
-  private final double fromValue;
-  private final double toValue;
-
-  public RingDensityFunction(int gridSize, int minRadius, int maxRadius, int blendRadius, double fromValue, double toValue) {
-    this.gridSize = gridSize;
-    this.minRadius = minRadius;
-    this.maxRadius = maxRadius;
-    this.blendRadius = blendRadius;
-    this.minRadiusSquared = minRadius * minRadius;
-    this.maxRadiusSquared = maxRadius * maxRadius;
-    this.fromValue = fromValue;
-    this.toValue = toValue;
-  }
-
   @Override
   public double sample(NoisePos pos) {
-    var centerX = MathUtils.getClosestMultiple(pos.blockX(), gridSize);
-    var centerZ = MathUtils.getClosestMultiple(pos.blockZ(), gridSize);
-    var distX = pos.blockX() - centerX;
-    var distZ = pos.blockZ() - centerZ;
+    var centerX = MathUtils.getClosestMultiple(pos.blockX() - (offsetGrid ? offsetX : 0), gridSize);
+    var centerZ = MathUtils.getClosestMultiple(pos.blockZ() - (offsetGrid ? offsetZ : 0), gridSize);
+    var distX = pos.blockX() - centerX - (offsetGrid ? 0 : offsetX);
+    var distZ = pos.blockZ() - centerZ - (offsetGrid ? 0 : offsetZ);
     var distSquared = distX * distX + distZ * distZ;
-    if (distSquared < minRadiusSquared || distSquared > maxRadiusSquared) {
+    if (distSquared < minRadius * minRadius || distSquared > maxRadius * maxRadius) {
       return fromValue;
     }
     if (blendRadius == 0) {
