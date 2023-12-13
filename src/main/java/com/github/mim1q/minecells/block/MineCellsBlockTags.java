1f71f535a059dfb8c05580f8f099dc9cd4147620
mim1q <mim1q.dev@gmail.com>
2023-12-13 11:08:44 +0100

    Improved return stones (block tag for targets + better target lookup)

--- src/main/java/com/github/mim1q/minecells/block/MineCellsBlockTags.java
+++ src/main/java/com/github/mim1q/minecells/block/MineCellsBlockTags.java
@@ -9,4 +9,5 @@ public class MineCellsBlockTags {
   public static final TagKey<Block> CONJUNCTIVIUS_BREAKABLE = TagKey.of(RegistryKeys.BLOCK, MineCells.createId("conjunctivius_breakable"));
   public static final TagKey<Block> ELEVATOR_CHAINS = TagKey.of(RegistryKeys.BLOCK, MineCells.createId("elevator_chains"));
   public static final TagKey<Block> TREE_ROOT_REPLACEABLE = TagKey.of(RegistryKeys.BLOCK, MineCells.createId("tree_root_replaceable"));
+  public static final TagKey<Block> RETURN_STONE_TARGETS = TagKey.of(RegistryKeys.BLOCK, MineCells.createId("return_stone_targets"));
 }
