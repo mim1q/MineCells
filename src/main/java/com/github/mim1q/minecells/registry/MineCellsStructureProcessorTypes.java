cc01a95d7fb44adf2a57c38f361c2994dbf02504
mim1q <mim1q.dev@gmail.com>
2024-01-29 12:15:41 +0100

    Updated the custom structure block processor

--- src/main/java/com/github/mim1q/minecells/registry/MineCellsStructureProcessorTypes.java
+++ src/main/java/com/github/mim1q/minecells/registry/MineCellsStructureProcessorTypes.java
@@ -1,6 +1,6 @@
 package com.github.mim1q.minecells.registry;
 
-import com.github.mim1q.minecells.world.processor.BetterRuleStructureProcessor;
+import com.github.mim1q.minecells.world.processor.SwitchBlockStructureProcessor;
 import com.mojang.serialization.Codec;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -8,12 +8,13 @@ import net.minecraft.structure.processor.StructureProcessor;
 import net.minecraft.structure.processor.StructureProcessorType;
 
 public class MineCellsStructureProcessorTypes {
-  public static final StructureProcessorType<BetterRuleStructureProcessor> BETTER_RULE = register(
-    "better_rule",
-    BetterRuleStructureProcessor.CODEC
+  public static final StructureProcessorType<SwitchBlockStructureProcessor> SWITCH_BLOCK = register(
+    "switch_block",
+    SwitchBlockStructureProcessor.CODEC
   );
 
-  public static void init() {}
+  public static void init() {
+  }
 
   static <P extends StructureProcessor> StructureProcessorType<P> register(String id, Codec<P> codec) {
     return Registry.register(Registries.STRUCTURE_PROCESSOR, "minecells:" + id, () -> codec);
