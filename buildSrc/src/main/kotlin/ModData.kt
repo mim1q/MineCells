380b3084061fa0c56bb64c711e064fc7286e912a
Mim1q <mim1q.dev@gmail.com>
2024-02-27 15:40:03 +0100

    Version bump

--- buildSrc/src/main/kotlin/ModData.kt
+++ buildSrc/src/main/kotlin/ModData.kt
@@ -1,8 +1,8 @@
 object ModData {
   const val GROUP = "com.github.mim1q"
   const val ID = "minecells"
-  const val VERSION = "1.7.0"
-  const val VERSION_TYPE = "beta"
+  const val VERSION = "1.7.1"
+  const val VERSION_TYPE = "release"
   val mcVersions = listOf("1.20.1")
   val dependencies = listOf("fabric-api", "owo-lib")
 }
