85e7be5c3643e09469c1131856bbe64895c28bfd
mim1q <mim1q.dev@gmail.com>
2024-04-07 14:07:53 +0200

    Updated dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,10 +2,10 @@ object Versions {
   const val MINECRAFT = "1.20.1"
 
   // Fabric
-  const val FABRIC_LOADER = "0.15.0"
-  const val FABRIC_API = "0.86.1+1.20.1"
+  const val FABRIC_LOADER = "0.15.9"
+  const val FABRIC_API = "0.92.0+1.20.1"
   const val YARN = "1.20.1+build.10"
-  const val LOOM = "1.1-SNAPSHOT"
+  const val LOOM = "1.6-SNAPSHOT"
 
   // Required / embedded dependencies
   const val OMEGA_CONFIG = "1.2.3-1.18.1"
