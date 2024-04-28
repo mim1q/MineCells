4feb3258d2042f44958627643d7a82b367071a1a
mim1q <mim1q.dev@gmail.com>
2024-04-28 13:39:40 +0200

    Fixed new recipe toasts appearing every time you reopen the world

--- src/main/java/com/github/mim1q/minecells/mixin/client/ClientAdvancementManagerMixin.java
+++ src/main/java/com/github/mim1q/minecells/mixin/client/ClientAdvancementManagerMixin.java
@@ -1,8 +1,6 @@
 package com.github.mim1q.minecells.mixin.client;
 
-import com.github.mim1q.minecells.client.gui.toast.CellCrafterRecipeToast;
 import com.github.mim1q.minecells.client.render.misc.AdvancementHintRenderer;
-import com.github.mim1q.minecells.registry.MineCellsRecipeTypes;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientAdvancementManager;
 import net.minecraft.network.packet.s2c.play.AdvancementUpdateS2CPacket;
@@ -25,15 +23,5 @@ public class ClientAdvancementManagerMixin {
     packet.getAdvancementIdsToRemove().forEach(
       id -> AdvancementHintRenderer.setAdvancementRendered(id, true)
     );
-
-    // Display toasts for unlocked Cell Crafter recipes
-    var world = client.world;
-    if (world == null) return;
-    var allRecipes = world.getRecipeManager().listAllOfType(MineCellsRecipeTypes.CELL_FORGE_RECIPE_TYPE);
-
-    packet.getAdvancementsToEarn().forEach((id, builder) -> {
-      var recipes = allRecipes.stream().filter(recipe -> recipe.requiredAdvancement().map(it -> it.equals(id)).orElse(false));
-      recipes.forEach(recipe -> client.getToastManager().add(new CellCrafterRecipeToast(recipe)));
-    });
   }
 }
