649525df83dcfd7baaf6cfec8ee30acd8e6d2c95
mim1q <mim1q.dev@gmail.com>
2024-04-28 13:40:06 +0200

    unused var

--- src/main/java/com/github/mim1q/minecells/mixin/client/ClientAdvancementManagerMixin.java
+++ src/main/java/com/github/mim1q/minecells/mixin/client/ClientAdvancementManagerMixin.java
@@ -1,20 +1,15 @@
 package com.github.mim1q.minecells.mixin.client;
 
 import com.github.mim1q.minecells.client.render.misc.AdvancementHintRenderer;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientAdvancementManager;
 import net.minecraft.network.packet.s2c.play.AdvancementUpdateS2CPacket;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientAdvancementManager.class)
 public class ClientAdvancementManagerMixin {
-  @Shadow @Final private MinecraftClient client;
-
   @Inject(method = "onAdvancements", at = @At("HEAD"))
   private void minecells$onAdvancements(AdvancementUpdateS2CPacket packet, CallbackInfo ci) {
     packet.getAdvancementsToEarn().forEach(
