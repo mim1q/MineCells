63bafe460eaeef9805a7647dd7b4abb34062780a
mim1q <mim1q.dev@gmail.com>
2024-10-21 15:45:00 +0200

    unused import

--- src/main/java/com/github/mim1q/minecells/registry/MineCellsStatusEffects.java
+++ src/main/java/com/github/mim1q/minecells/registry/MineCellsStatusEffects.java
@@ -8,7 +8,6 @@ import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.stat.Stat;
 
 
 public class MineCellsStatusEffects {
