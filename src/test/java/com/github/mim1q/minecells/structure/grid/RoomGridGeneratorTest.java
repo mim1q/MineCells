da7d751a324bf3dd2df76832bc4e64b91e44a36f
mim1q <mim1q.dev@gmail.com>
2025-03-04 23:36:42 +0100

    Updated and randomized test (unreproducibility here we come)

--- src/test/java/com/github/mim1q/minecells/structure/grid/RoomGridGeneratorTest.java
+++ src/test/java/com/github/mim1q/minecells/structure/grid/RoomGridGeneratorTest.java
@@ -4,29 +4,44 @@ import com.github.mim1q.minecells.structure.grid.generator.*;
 import net.minecraft.Bootstrap;
 import net.minecraft.SharedConstants;
 import net.minecraft.util.math.random.Random;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RoomGridGeneratorTest {
   private static final int TEST_COUNT = 20_000;
 
-  @Test
-  public void testAllGenerators() {
+  @BeforeAll
+  public static void setup() {
     SharedConstants.createGameVersion();
     Bootstrap.initialize();
+  }
 
-    var generators = List.of(
-      new PrisonGridGenerator(0, 0),
-      new BetterPromenadeGridGenerator(0, 0),
-      new RampartsGridGenerator(0, 0),
-      new BlackBridgeGridGenerator(0, 0),
-      new InsufferableCryptGridGenerator(0, 0)
-    );
 
-    generators.forEach(RoomGridGeneratorTest::testRoomGridGenerator);
+  @Test
+  public void testPrisonGenerator() {
+    testRoomGridGenerator(new PrisonGridGenerator(0, 0));
+  }
+
+  @Test
+  public void testBetterPromenadeGenerator() {
+    testRoomGridGenerator(new BetterPromenadeGridGenerator(0, 0));
+  }
+
+  @Test
+  public void testRampartsGenerator() {
+    testRoomGridGenerator(new RampartsGridGenerator(0, 0));
+  }
+
+  @Test
+  public void testBlackBridgeGenerator() {
+    testRoomGridGenerator(new BlackBridgeGridGenerator(0, 0));
+  }
+
+  @Test
+  public void testInsufferableCryptGenerator() {
+    testRoomGridGenerator(new InsufferableCryptGridGenerator(0, 0));
   }
 
   private static void testRoomGridGenerator(GridPiecesGenerator.RoomGridGenerator generator) {
@@ -34,17 +49,15 @@ public class RoomGridGeneratorTest {
     var generatorName = generator.getClass().getSimpleName();
 
     for (int i = 0; i < TEST_COUNT; ++i) {
-      random.setSeed(i);
+      random.setSeed(new java.util.Random().nextInt(Integer.MAX_VALUE));
       generator.generateForTesting(random);
       var rooms = generator.usedPositions;
       for (var room : rooms) {
         assertTrue(
           (room.getX() >= 0 && room.getX() < 64 && room.getZ() >= 0 && room.getZ() < 64),
-          "Invalid room placement in generator: " + generatorName
+          "Invalid room placement in generator: " + generatorName + ", at: " + room
         );
       }
     }
-
-    System.out.println("Successfully tested " + generatorName + " for invalid room placement");
   }
 }
