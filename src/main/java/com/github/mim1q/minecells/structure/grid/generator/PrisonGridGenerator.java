c9d1859c704ce33bff218a11e7854d8ac976e0dc
mim1q <mim1q.dev@gmail.com>
2025-03-24 01:27:13 +0100

    Amazing dungeon layout. The designer must be very cool

--- src/main/java/com/github/mim1q/minecells/structure/grid/generator/PrisonGridGenerator.java
+++ src/main/java/com/github/mim1q/minecells/structure/grid/generator/PrisonGridGenerator.java
@@ -17,6 +17,7 @@ public class PrisonGridGenerator extends MultipartGridGenerator {
   private static final Identifier STAIRS = id("stairs");
   private static final Identifier STRAIGHT = id("straight");
   private static final Identifier TURN = id("turn");
+  private static final Identifier CURVE = id("curve");
 
   private static final Identifier TERMINAL = id("terminal");
 
@@ -35,11 +36,29 @@ public class PrisonGridGenerator extends MultipartGridGenerator {
     addRoom(room(spawnCursor.stepLeft(), SPAWN_OUTSIDE_NEAR).offset(0, -2, 0));
     addRoom(room(spawnCursor.stepLeft(), SPAWN_OUTSIDE_FAR).offset(0, -2, 0));
 
-    addFloor(random, cursor, 4);
+    addFloor(random, cursor, random.nextBetween(4, 5));
   }
 
   private Vec3iCursor addCorridor(Random random, Vec3iCursor cursor, boolean allowTurns, boolean hasEnd, int length) {
     for (int i = 0; i < length; ++i) {
+      if (random.nextFloat() < 0.2f && allowTurns && i < length - 1 && i > 0) {
+        var left = random.nextBoolean();
+        if (left) {
+          addRoom(room(cursor.forward(), CURVE).rotation(cursor.getRotation().rotate(BlockRotation.COUNTERCLOCKWISE_90)));
+          for (var j = 0; j < random.nextInt(3); ++j) {
+            addRoom(room(cursor.stepLeft(), STRAIGHT).rotation(cursor.getRotation().rotate(BlockRotation.CLOCKWISE_90)));
+          }
+          addRoom(room(cursor.stepLeft(), CURVE).rotation(cursor.getRotation().rotate(BlockRotation.CLOCKWISE_90)));
+        } else {
+          addRoom(room(cursor.forward(), CURVE).rotation(cursor.getRotation().rotate(BlockRotation.CLOCKWISE_180)));
+          for (var j = 0; j < random.nextInt(3); ++j) {
+            addRoom(room(cursor.stepRight(), STRAIGHT).rotation(cursor.getRotation().rotate(BlockRotation.COUNTERCLOCKWISE_90)));
+          }
+          addRoom(room(cursor.stepRight(), CURVE).rotation(cursor.getRotation().rotate(BlockRotation.NONE)));
+        }
+        continue;
+      }
+
       var turn = allowTurns && random.nextBoolean();
       var rotation = (random.nextBoolean() ? BlockRotation.NONE : BlockRotation.CLOCKWISE_180);
       addRoom(room(cursor.forward(), turn ? TURN : STRAIGHT)
@@ -47,7 +66,7 @@ public class PrisonGridGenerator extends MultipartGridGenerator {
       );
 
       if (turn) {
-        addCorridor(random, cursor.split().turn(rotation).turnRight(), false, true, 5);
+        addCorridor(random, cursor.split().turn(rotation).turnRight(), false, true, random.nextBetween(2, 4));
       }
     }
     if (hasEnd) {
@@ -58,14 +77,15 @@ public class PrisonGridGenerator extends MultipartGridGenerator {
   }
 
   private void addFloor(Random random, Vec3iCursor cursor, int index) {
-    var endCursor = addCorridor(random, cursor, true, false, 10);
+    var endCursor = addCorridor(random, cursor, true, false, random.nextBetween(3, 5));
+    endCursor.forward();
     if (index > 0) {
       var newCursor = endCursor.split();
       addRoom(room(newCursor.down(), STAIRS));
-//      addCorridor(random, newCursor, true, true, 3);
+      addCorridor(random, newCursor.split(), true, true, random.nextBetween(0, 2));
       addFloor(random, newCursor.turn(BlockRotation.CLOCKWISE_180), index - 1);
     }
-    addCorridor(random, endCursor, true, true, 10);
+    addCorridor(random, endCursor, true, true, random.nextBetween(0, 2));
   }
 
   @Override
