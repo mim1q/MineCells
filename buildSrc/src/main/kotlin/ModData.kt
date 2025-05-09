268e1469328d254eae51703dd8169931186de480
mim1q <mim1q.dev@gmail.com>
2025-05-09 14:36:33 +0200

    Fixed advancements not loading

--- buildSrc/src/main/kotlin/ModData.kt
+++ buildSrc/src/main/kotlin/ModData.kt
@@ -1,8 +1,8 @@
 object ModData {
   const val GROUP = "com.github.mim1q"
   const val ID = "minecells"
-  const val VERSION = "1.9.2"
-  const val VERSION_TYPE = "release"
+  const val VERSION = "2.0.0"
+  const val VERSION_TYPE = "alpha"
   val mcVersions = listOf("1.20.1")
   val dependencies = listOf("fabric-api", "owo-lib")
 }
