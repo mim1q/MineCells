b776864ccaec6d040dc3e17ee90623957b92ae73
mim1q <mim1q.dev@gmail.com>
2025-09-23 20:32:24 +0200

    Recipes giving more than 1 item fixed

--- buildSrc/src/main/kotlin/ModData.kt
+++ buildSrc/src/main/kotlin/ModData.kt
@@ -1,8 +1,8 @@
 object ModData {
   const val GROUP = "com.github.mim1q"
   const val ID = "minecells"
-  const val VERSION = "2.0.0"
-  const val VERSION_TYPE = "alpha"
+  const val VERSION = "2.0.1"
+  const val VERSION_TYPE = "beta"
   val mcVersions = listOf("1.21.1")
   val dependencies = listOf("fabric-api", "owo-lib")
 }
