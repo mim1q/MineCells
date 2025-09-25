6416b590a97adec6e07c87a8577be517f9f9a64e
mim1q <mim1q.dev@gmail.com>
2025-09-25 23:53:48 +0200

    Updated the Ramparts spawn area to prevent crash (workaround)

--- buildSrc/src/main/kotlin/ModData.kt
+++ buildSrc/src/main/kotlin/ModData.kt
@@ -1,7 +1,7 @@
 object ModData {
   const val GROUP = "com.github.mim1q"
   const val ID = "minecells"
-  const val VERSION = "2.0.2"
+  const val VERSION = "2.0.3"
   const val VERSION_TYPE = "beta"
   val mcVersions = listOf("1.21.1")
   val dependencies = listOf("fabric-api", "owo-lib")
